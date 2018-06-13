74e43c6ee23f222068f4eefc7741bea128113f40
Anik Sarker <aniksarkerbuet1997@gmail.com>
2018-06-13 12:27:55 +0600

    Update Trie.cpp

--- DS/Trie.cpp
+++ DS/Trie.cpp
@@ -2,25 +2,28 @@
 //Space Complexity : O(Sum of length of all strings)
 
 struct NODE{
-	NODE* Child[2];
+    NODE* Child[2];
     int SIZE;
-	NODE(){Child[0]=Child[1]=NULL; SIZE=0;}
+    NODE(){
+        Child[0]=Child[1]=NULL;
+        SIZE=0;
+    }
 };
 
 NODE* root;
 void INSERT(int n){
-	NODE *Cur=root;
-	for(int i=MAX-1;i>=0;i--){
+    NODE *Cur=root;
+    for(int i=MAX-1;i>=0;i--)    {
         int ID=check(n,i);
-		if(Cur->Child[ID]==NULL) Cur->Child[ID]=new NODE();
+        if(Cur->Child[ID]==NULL) Cur->Child[ID]=new NODE();
         Cur=Cur->Child[ID];
-		Cur->SIZE++;
-	}
+        Cur->SIZE++;
+    }
 }
 
 int QUERY(int L){
-	NODE *Cur=root;
-	int Ans=0;
-	for(int i=MAX-1;i>=0;i--){......}
+    NODE *Cur=root;
+    int Ans=0;
+    for(int i=MAX-1; i>=0; i--){......}
     return Ans;
 }
