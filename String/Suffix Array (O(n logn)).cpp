aa056ac097b1d92c993913931b6cd34e270a8c56
Anik Sarker <aniksarkerbuet1997@gmail.com>
2018-06-27 11:44:44 +0600

    Update Suffix Array (O(n logn)).cpp

--- String/Suffix Array (O(n logn)).cpp
+++ String/Suffix Array (O(n logn)).cpp
@@ -1,10 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
 #define MAX 100005
 
 int wa[MAX],wb[MAX],wv[MAX],Ws[MAX];
 int cmp(int *r,int a,int b,int l) {return r[a]==r[b] && r[a+l]==r[b+l];}
 
-//(1-indexed) sa[i] = starting position of ith lexicographically smallest suffix in s
-//(0-indexed) Rank[i] = lexicographical rank of s[i-1....n-1] (ith suffix by position)
+//(1-indexed) sa[i] = starting position (0...n-1) of ith lexicographically smallest suffix in s
+//(0-indexed) Rank[i] = lexicographical rank of s[i....n-1] ((i+1)th suffix by position)
 //LCP[i] = longest common prefix of sa[i] & sa[i-1]
 int sa[MAX],Rank[MAX],LCP[MAX];
 
@@ -35,4 +37,14 @@ void buildLCP(string s,int *sa,int n){
     for(i=0; i<n; LCP[Rank[i++]]=k)
         for(k?k--:0, j=sa[Rank[i]-1]; s[i+k]==s[j+k]; k++);
     return;
+}
+
+int main(){
+    string s="abababab";
+    int n=s.size();
+    buildSA(s,sa,n+1,130);
+    buildLCP(s,sa,n);
+    for(int i=1;i<=n;i++) cout<<sa[i]<<" "; cout<<endl;
+    for(int i=0;i<n;i++) cout<<Rank[i]<<" "; cout<<endl;
+    for(int i=1;i<=n;i++) cout<<LCP[i]<<" ";
 }
