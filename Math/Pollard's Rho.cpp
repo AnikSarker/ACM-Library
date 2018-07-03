8bfe6671122c347ece33463fc9a5dd7dd469c2ce
Anik Sarker <aniksarkerbuet1997@gmail.com>
2018-07-03 12:21:31 +0600

    Update Pollard's Rho.cpp

--- Math/Pollard's Rho.cpp
+++ Math/Pollard's Rho.cpp
@@ -1,5 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define MAX 100005
+#define ll long long int
+#define pii pair<ll,int>
 
 ll bigMod(ll n,ll r,ll Mod){
     if(r==0) return 1LL;
@@ -9,7 +12,7 @@ ll bigMod(ll n,ll r,ll Mod){
     return ret;
 }
 
-//MILLER-RABIN
+//Miller-Rabin
 bool witness(ll wit,ll n){
   if(wit>=n) return false;
 
@@ -19,15 +22,15 @@ bool witness(ll wit,ll n){
   wit=bigMod(wit,t,n);
   if(wit==1 || wit==n-1) return false;
 
-  for(int i=1;i<s;i++) {
+  for(int i=1;i<s;i++){
     wit=(wit*wit)%n;
     if(wit==1) return true;
-    if(wit == n - 1) return false;
+    if(wit==n - 1) return false;
   }
   return true;
 }
 
-//Is n Prime ---> true/false
+//Is n prime?
 bool miller(ll n){
   if(n==2) return true;
   if(n%2==0 || n<2) return false;
@@ -35,8 +38,8 @@ bool miller(ll n){
   return true;
 }
 
-// POLARD'S RHO
-// a - parameter, shall not be equal to 0 or -2.
+// Pollard's Rho
+// a must not equal 0 or -2.
 // returns a divisor, a proper one when succeeded, equal to n if failed
 // in case of failure, change a
 ll rho(ll n,ll a) {
@@ -67,3 +70,56 @@ void factorize(ll n,vector<ll> &x) {
 }
 
 vector<ll>factorize(ll n) {vector<ll>x; factorize(n, x); return x;}
+
+vector<pii>Factors;
+vector<ll>Divisors;
+void findDiv(int pos,ll val){
+    if(pos<0) {Divisors.push_back(val); return;}
+    ll Now=1;
+    for(int i=0;i<=Factors[pos].second;i++){
+        findDiv(pos-1,val*Now);
+        Now=Now*Factors[pos].first;
+    }
+}
+
+void findAllDiv(ll n){
+    vector<ll>now=factorize(n);
+    sort(now.begin(),now.end());
+
+    Factors.clear();
+    Divisors.clear();
+    int Count=1;
+    for(int i=1;i<now.size();i++){
+        if(now[i]==now[i-1]) Count++;
+        else {Factors.push_back({now[i-1],Count}); Count=1;}
+    }
+    Factors.push_back({now.back(),Count});
+    findDiv(Factors.size()-1,1);
+}
+
+ll a[MAX];
+set<ll>SET;
+
+int main(){
+    int n,k;
+    scanf("%d %d",&n,&k);
+
+    for(int i=1;i<=n;i++) scanf("%lld",&a[i]);
+
+    random_shuffle(a+1,a+n+1);
+    for(int i=1;i<=min(n,10);i++){
+        if(a[i]==1) SET.insert(1);
+        else{findAllDiv(a[i]); for(ll xx: Divisors) SET.insert(xx);}
+    }
+
+    ll Ans=1;
+    for(ll now : SET){
+        int Count=0;
+        for(int i=n;i>=1;i--){
+            if(a[i]%now!=0) Count++;
+            if(Count>k) break;
+        }
+        if(Count<=k) Ans=now;
+    }
+    cout<<Ans<<endl;
+}
