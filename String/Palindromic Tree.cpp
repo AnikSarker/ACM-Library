0c77e48af05c4e98c16698899b8c3de4d54df454
Anik Sarker <aniksarkerbuet1997@gmail.com>
2018-07-05 10:37:36 +0600

    Update Palindromic Tree.cpp

--- String/Palindromic Tree.cpp
+++ String/Palindromic Tree.cpp
@@ -8,7 +8,8 @@ using namespace std;
 struct node{
     int Next[26];
     int Len;        //Length of the palindrome represented by current node
-    int Count;      //Number of palindromic suffixes of the palindrome represented by current node 
+    int Count;      //Number of palindromic suffixes of the palindrome represented by current node
+    int Occurrence; //Total number of occurrence of this node
     int SuffLink;   //Links such node w such that the palindrome represented by node w is the largest
                     //palindrome which is a proper suffix of the palindrome represented by current node
 };
@@ -16,10 +17,10 @@ struct node{
 string s;
 node Tree[MAX];
 int Suff;           // Points to Current (initially previous) node in palindromic tree
-int num;            // num = Total number of node in palindromic tree
-                    // num = 1 denotes node 1 (root with Len -1)
-                    // num = 2 denotes node 2 (root with Len  0)
-bool addLetter(int pos){
+int Size;           // Size = Total number of node in palindromic tree
+                    // Size = 1 denotes node 1 (root with Len -1)
+                    // Size = 2 denotes node 2 (root with Len  0)
+bool AddLetter(int pos){
     int Cur=Suff;
     int CurLen=0;
     int Let=s[pos]-'a';
@@ -29,37 +30,38 @@ bool addLetter(int pos){
         if(pos-1 >= CurLen && s[pos-1-CurLen]==s[pos]) break;
         Cur=Tree[Cur].SuffLink;
     }
+    Tree[Cur].Occurrence++;
 
     if(Tree[Cur].Next[Let]) {Suff=Tree[Cur].Next[Let]; return false;}
-    num++;
-    Suff=num;
-    Tree[num].Len = Tree[Cur].Len + 2;
-    Tree[Cur].Next[Let] = num;
+    Size++;
+    Suff=Size;
+    Tree[Size].Len=Tree[Cur].Len + 2;
+    Tree[Cur].Next[Let]=Size;
 
-    if(Tree[num].Len==1) {Tree[num].SuffLink=2; Tree[num].Count=1; return true;}
+    if(Tree[Size].Len==1) {Tree[Size].SuffLink=2; Tree[Size].Count=1; return true;}
 
     while(true){
         Cur=Tree[Cur].SuffLink;
         CurLen=Tree[Cur].Len;
         if(pos-1 >= CurLen && s[pos-1-CurLen]==s[pos]){
-            Tree[num].SuffLink=Tree[Cur].Next[Let];
+            Tree[Size].SuffLink=Tree[Cur].Next[Let];
             break;
         }
     }
 
-    Tree[num].Count=1+Tree[Tree[num].SuffLink].Count;
+    Tree[Size].Count=1+Tree[Tree[Size].SuffLink].Count;
     return true;
 }
 
-void initTree(){
-    num = 2; Suff = 2;
-    Tree[1].Len = -1; Tree[1].SuffLink = 1;
-    Tree[2].Len = 0;  Tree[2].SuffLink = 1;
+void InitTree(){
+    Size=2; Suff=2;
+    Tree[1].Len=-1; Tree[1].SuffLink=1;
+    Tree[2].Len=0;  Tree[2].SuffLink=1;
 }
 
 int main(){
     //memset(Tree,0,sizeof(Tree));
-    //initTree();
-    //for(int i=0;i<n;i++) addLetter(i);
-    //distinct palindrome count = num - 2
+    //InitTree();
+    //for(int i=0;i<n;i++) AddLetter(i);
+    //distinct palindrome count = Size - 2
 }
