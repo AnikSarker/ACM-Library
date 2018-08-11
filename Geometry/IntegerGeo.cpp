f291895e118e44f8d6f5824607f6a216014b830c
Anik Sarker <aniksarkerbuet1997@gmail.com>
2018-08-12 01:31:02 +0600

    Update IntegerGeo.cpp

--- Geometry/IntegerGeo.cpp
+++ Geometry/IntegerGeo.cpp
@@ -7,6 +7,6 @@ struct Line {
     Line(int x=0,int y=0,int z=0){
         int G=__gcd(x,y);            //will equal gcd(x,y,z)
         a=x/G; b=y/G; c=z/G;
-        if(a<0) {a=-a; b=-b; c=-c;}  //No case needed for a=0
+        if(a<0) {a=-a; b=-b; c=-c;}  //a=0 case auto-handled
     }
 };
