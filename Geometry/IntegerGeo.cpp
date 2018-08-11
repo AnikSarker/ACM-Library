793ecedaf08f0f18230d306c0fa9b93d844805e3
Anik Sarker <aniksarkerbuet1997@gmail.com>
2018-08-12 01:05:41 +0600

    Create IntegerGeo.cpp

---
+++ Geometry/IntegerGeo.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Line {
+    int a,b,c;
+    Line(){}
+    Line(int x=0,int y=0,int z=0){
+        int G=__gcd(x,y);            //will equal gcd(x,y,z)
+        a=x/G; b=y/G; c=z/G;
+        if(a<0) {a=-a; b=-b; c=-c;}  //No case needed for a=0
+    }
+};
