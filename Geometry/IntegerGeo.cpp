859d98deed53975613a0ee9f033334b6fe576ae4
Anik Sarker <aniksarkerbuet1997@gmail.com>
2018-08-17 02:49:33 +0600

    Update IntegerGeo.cpp

--- Geometry/IntegerGeo.cpp
+++ Geometry/IntegerGeo.cpp
@@ -1,11 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define ll long long int
+
+struct Fraction{
+    ll m,n;
+    Fraction(){}
+    Fraction(ll x,ll y) {ll g=__gcd(x,y); m=x/g; n=y/g; if(n<0) {m=-m,n=-n;} }
+    Fraction operator + (const Fraction& u) const {return Fraction(m*u.n+u.m*n,n*u.n); }
+    Fraction operator - (const Fraction& u) const {return Fraction(m*u.n-u.m*n,n*u.n); }
+    Fraction operator * (const Fraction& u) {return Fraction(m*u.m,n*u.n); }
+    Fraction operator / (const Fraction& u) {return Fraction(m*u.n,n*u.m); }
+};
 
 struct Line {
-    int a,b,c;
+    ll a,b,c;
     Line(){}
-    Line(int x=0,int y=0,int z=0){
-        int G=__gcd(x,y);            //will equal gcd(x,y,z)
+    Line(ll x=0,ll y=0,ll z=0){
+        ll G=__gcd(x,y);            //will equal gcd(x,y,z)
         a=x/G; b=y/G; c=z/G;
         if(a<0) {a=-a; b=-b; c=-c;}  //a=0 case auto-handled
     }
