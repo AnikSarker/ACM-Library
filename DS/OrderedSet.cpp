70243ced600d1d9ce7e7e9f68268270ef5b3741d
Anik Sarker <aniksarkerbuet1997@gmail.com>
2018-08-26 02:38:21 +0600

    Update OrderedSet.cpp

--- DS/OrderedSet.cpp
+++ DS/OrderedSet.cpp
@@ -6,9 +6,7 @@ using namespace std;
 using namespace __gnu_pbds;
 
 typedef tree<
-int,
-null_type,
-less<int>,
+int,null_type,less<int>,
 rb_tree_tag,
 tree_order_statistics_node_update>
 ordered_set;
