0bb80f9644ac3c3958e3334f4e4f61719e05ae70
Double-O <hrdd03@gmail.com>
2018-08-26 02:37:17 +0600

    Create OrderedSet.cpp

---
+++ DS/OrderedSet.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+#include <ext/pb_ds/detail/standard_policies.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef tree<
+int,
+null_type,
+less<int>,
+rb_tree_tag,
+tree_order_statistics_node_update>
+ordered_set;
+ordered_set xx;
