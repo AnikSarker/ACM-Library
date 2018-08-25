2637a61217a026822b4003cfdb897e6e6bf2e03c
Double-O <hrdd03@gmail.com>
2018-08-25 13:35:13 +0600

    Update Matrix Exponentiation.cpp

--- Math/Matrix Exponentiation.cpp
+++ Math/Matrix Exponentiation.cpp
@@ -1,26 +1,47 @@
 //Matrix Exponentiation
-int n;
-struct Matrix{int m[MAX][MAX];};
+typedef long long ll;
+const ll mod = 1e9 + 7;
+ll mod2 = mod * mod * 5;
+const ll MAX = 105;
+
+struct Matrix{
+    ll row, col;
+    ll m[MAX][MAX];
+};
 
 Matrix Multiply(Matrix A,Matrix B){
     Matrix ans;
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=n;j++){
+
+    ans.row = A.row;
+    ans.col = B.col;
+
+    for(ll i=1;i<=A.row;i++){
+        for(ll j=1;j<=B.col;j++){
             ans.m[i][j]=0;
-            for(int k=1;k<=n;k++){
+            ll sm = 0;
+            for(ll k=1;k<=A.col;k++){
+                sm+=(A.m[i][k]*B.m[k][j]);
+                if(sm >= mod2) sm -= mod2;
                 ans.m[i][j]+=A.m[i][k]*B.m[k][j];
+//                ans.m[i][j] = ans.m[i][j] % mod;
+
             }
+            ans.m[i][j] = sm % mod;
         }
     }
     return ans;
 }
 
-Matrix Power(Matrix mat,int p){
+Matrix Power(Matrix mat,ll p){
     Matrix res;
     Matrix ans;
 
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=n;j++){
+    ans.row = res.row = mat.row;
+    ans.col = res.col = mat.col;
+    ll n = ans.row;
+
+    for(ll i=1;i<=n;i++){
+        for(ll j=1;j<=n;j++){
             ans.m[i][j]=0;
             res.m[i][j]=mat.m[i][j];
         }
