b6506a8ab3b4f676b7bda66ddbe1f1cc6dc091a3
Anik Sarker <aniksarkerbuet1997@gmail.com>
2018-08-29 09:26:00 +0600

    Create Dynamic Segment Tree.cpp

---
+++ DS/Dynamic Segment Tree.cpp
@@ -0,0 +1,65 @@
+#define mid ((lo+hi)/2)
+
+struct item{
+    int value;
+    int lazy;
+    item* Left ;
+    item* Right ;
+    item(){value=0; Left=Right=NULL;}
+};
+item* root;
+
+int query(item* node, int lo, int hi, int i, int j){
+    if (!node || i>hi || j<lo) return 0;
+    if (lo>=i && hi<=j) return node->value;
+    int p1=query(node->Left,lo,mid,i,j);
+    int p2=query(node->Right,mid+1,hi,i,j);
+    return p1+p2;
+}
+
+void update(item* node, int lo, int hi, int i, int val){
+    if (i>hi || i<lo) return;
+    if (lo==hi) {node->value=val; return;}
+    if(!node->Left) node->Left=new item();
+    if(!node->Right) node->Right=new item();
+
+    update(node->Left,lo,mid,i,val);
+    update(node->Right,mid+1,hi,i,val);
+    node->value=node->Left->value + node->Right->value;
+}
+
+//lazy propagation
+void lazyPropagation(item* node,int lo,int hi){
+    if(node->lazy){
+        node->value+=(hi-lo+1)*node->lazy;
+        if(lo != hi) {node->Left->lazy+=node->lazy; node->Right->lazy+=node->lazy;}
+        node->lazy=0;
+    }
+}
+
+void updateRange(item* node, int lo, int hi, int i, int j, int val){
+    if(lo>hi) return;
+    else if(lo>j || hi<i) return;
+    if(!node->Left) node->Left=new item();
+    if(!node->Right) node->Right=new item();
+
+    lazyPropagation(node,lo,hi);
+    if(lo>=i && hi<=j) {node->lazy+=val;  lazyPropagation(node,lo,hi);  return;}
+
+    updateRange(node->Left,lo,mid,i,j,val);
+    updateRange(node->Right,mid+1,hi,i,j,val);
+    node->value=node->Left->value + node->Right->value;
+}
+
+int queryRange(item* node,int lo,int hi,int i,int j){
+    if(lo>hi) return 0;
+    else if(lo>j || hi<i) return 0;
+    if(!node->Left) node->Left=new item();
+    if(!node->Right) node->Right=new item();
+
+    lazyPropagation(node,lo,hi);
+    if(lo>=i && hi <= j) return node->value;
+    int p1=queryRange(node->Left,lo,mid,i,j);
+    int p2=queryRange(node->Right,mid+1,hi,i,j);
+    return p1+p2;
+}
