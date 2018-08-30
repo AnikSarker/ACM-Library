6046e295ee5572478f3b7e828f4490052356234b
Anik Sarker <aniksarkerbuet1997@gmail.com>
2018-08-31 05:52:02 +0600

    Update Maximum Xor Subset.cpp

--- Math/Maximum Xor Subset.cpp
+++ Math/Maximum Xor Subset.cpp
@@ -1,38 +1,42 @@
-// Taken From Rezwan Arefin
-// Gaussian Elimination Online 
-struct maxxor {
-	int best[32], msb;
-	maxxor() { memset(best, -1, sizeof best); }
-	void add(int x) { 
-		while(x > 0) {
-			msb = 31 - __builtin_clz(x); // clzll for ll
-			if(best[msb] == -1) {
-				best[msb] = x; break;
-			} else x = x ^ best[msb]; 
-		}
-	}
-	int get(int ret = 0) {
-		for(int i = 31; i >= 0; i--) {
-			if(best[i] != -1) 
-				ret = max(ret, ret ^ best[i]);
-		} return ret;
-	}
+#include<bits/stdc++.h>
+using namespace std;
+#define MAX 100005
+
+struct maxxor{
+    int best[32], msb;
+    maxxor(){memset(best, -1, sizeof best);}
+    void add(int x){
+        while(x > 0){
+            msb = 31 - __builtin_clz(x); // clzll for ll
+            if(best[msb] == -1){
+                best[msb] = x;
+                break;
+            }
+            else x = x ^ best[msb];
+        }
+    }
+    int get(int ret = 0){
+        for(int i = 31; i >= 0; i--){
+            if(best[i] != -1) ret = max(ret, ret ^ best[i]);
+        }
+        return ret;
+    }
 } ds;
 
-// Gaussian Elimination Offline 
+// Gaussian Elimination Offline
 
-int a[maxn], n;
-int maxxor() {
-	int r = 0, ret = 0;
-	for(int c = 31; c >= 0; c--) {
-		int idx = -1;
-		for(int i = r; i < n && idx < 0; i++) 
-			if(a[i] >> c & 1) idx = i;
-		if(idx == -1) continue; 
-		swap(a[r], a[idx]); 
-		for(int i = 0; i < n; i++) if(i != r) 
-			if(a[i] >> c & 1) a[i] ^= a[r];
-		r++;
-	}
-	for(int i = 0; i < n; i++) ret = max(ret, ret ^ a[i]);
+int a[MAX], n;
+int maxxor(){
+    int r = 0, ret = 0;
+    for(int c = 31; c >= 0; c--){
+        int idx = -1;
+        for(int i = r; i < n && idx < 0; i++)
+            if(a[i] >> c & 1) idx = i;
+        if(idx == -1) continue;
+        swap(a[r], a[idx]);
+        for(int i = 0; i < n; i++) if(i != r)
+                if(a[i] >> c & 1) a[i] ^= a[r];
+        r++;
+    }
+    for(int i = 0; i < n; i++) ret = max(ret, ret ^ a[i]);
 }
