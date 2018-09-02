53e3f8edeaae7fdedd22b7505cc535b31871a530
Anik Sarker <aniksarkerbuet1997@gmail.com>
2018-09-02 13:16:48 +0600

    Create NTT Applications - Stirling Number.cpp

---
+++ Math/NTT Applications - Stirling Number.cpp
@@ -0,0 +1,18 @@
+NTT ntt(mod);
+vector<ll>v[MAX];
+
+//Stirling (n,k) = co-eff of x^k in x*(x+1)*(x+2)*....(x+n-1) 
+int Stirling1(int n, int r) {
+	int nn = 1; while(nn < n) nn <<= 1;
+
+	for(int i = 0; i < n; ++i) {v[i].push_back(i); v[i].push_back(1);}
+	for(int i = n; i < nn; ++i) v[i].push_back(1);
+
+	for(int j = nn; j > 1; j >>= 1){
+		int hn = j >> 1;
+		for(int i = 0; i < hn; ++i){
+			ntt.multiply(v[i], v[i + hn], v[i]);
+		}
+	}
+	return v[0][r];
+}
