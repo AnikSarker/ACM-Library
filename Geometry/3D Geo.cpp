c4b33d5bb00e9e48360197ba370068056c145184
Anik Sarker <aniksarkerbuet1997@gmail.com>
2018-09-05 19:40:34 +0600

    Create 3D Geo.cpp

---
+++ Geometry/3D Geo.cpp
@@ -0,0 +1,11 @@
+struct Pyramid{
+    int n;    // number of side of the pyramid
+    double l; // length of each side
+    double ang;
+
+    Pyramid(int n, double l) {this->n=n; this->l=l; ang=pi/n;}
+
+    double getBaseArea() {return l * l * n / (4* tan(ang));}
+    double getHeight() {return l * sqrt(1 - 1 / (4*sin(ang)*sin(ang)) );}
+    double getVolume() {return getBaseArea() * getHeight() / 3;}
+};
