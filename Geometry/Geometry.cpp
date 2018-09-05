c76c33e43f14eeb15412768b0d096b90f0486f1b
Anik Sarker <aniksarkerbuet1997@gmail.com>
2018-09-05 19:11:26 +0600

    Update Geometry.cpp

--- Geometry/Geometry.cpp
+++ Geometry/Geometry.cpp
@@ -64,6 +64,20 @@ struct Circle {
     double sector(double alpha) {return r * r * 0.5 * (alpha - sin(alpha));}
 };
 
+struct Star{
+    int n;    // number of side of the star
+    double r; // radius of the circum-circle
+    Star(int n,double r) {this->n=n; this->r=r;}
+
+    double getArea(){
+        double theta=pi/n;
+        double s=2*r*sin(theta);
+        double R=0.5*s/tan(theta);
+        double a=0.5*n*s*R;
+        double a2=0.25*s*s/tan(1.5*theta);
+        return a-n*a2;
+    }
+};
 
 namespace Punctual {
     double getDistance (Point a, Point b) { double x=a.x-b.x, y=a.y-b.y; return sqrt(x*x + y*y); }
