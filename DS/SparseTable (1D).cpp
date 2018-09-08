e965d33b684733c8976541973550aa0b43a75375
Anik Sarker <aniksarkerbuet1997@gmail.com>
2018-09-08 20:39:41 +0600

    Update and rename SparseTable (1D+2D).cpp to SparseTable (1D).cpp

---
+++ DS/SparseTable (1D).cpp
@@ -0,0 +1,20 @@
+//1D SparseTable
+int n;
+int A[MAX];
+int Log[MAX];
+int M[MAX][MAXlogN];
+
+void buildSparse(){
+    for(int i=1;i<=n;i++) M[i][0]=A[i];
+    for(int i=2;i<=n;i++) Log[i]=Log[i-1] + ((i&(i-1)) == 0);
+    
+    for(int j=1; (1<<j)<=n ;j++){
+        for(int i=1; (i+ (1<<j)-1) <=n; i++)
+            M[i][j]=max(M[i][j-1],M[i + (1<<(j-1))][j - 1]);
+    }
+}
+
+int Query(int i,int j){
+    int k=Log[j-i+1];
+    return max(M[i][k],M[j-(1<<k)+1][k]);
+}
