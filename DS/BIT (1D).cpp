5b3acf1abe9af462b0a19d132c0db69c9a048b56
Anik Sarker <aniksarkerbuet1997@gmail.com>
2018-09-08 20:24:11 +0600

    Update and rename BIT (1D+2D).cpp to BIT (1D).cpp

--- DS/BIT (1D).cpp
+++ DS/BIT (1D).cpp
@@ -19,25 +19,3 @@ int LowerBound(int v){
     //pos = maximal x such that Sum(x) < v
     return indx+1; //+1 for LowerBound
 }
-
-//2D BIT
-int BIT[MAX][MAX];
-void update(int x,int y,int val){  //A[x][y]+=val
-    int xx=x;
-    while(xx<MAX){
-        int yy=y;
-        while(yy<MAX) {BIT[xx][yy]+=val;yy+=(yy&-yy);}
-        xx+=(xx&-xx);
-    }
-}
-
-int sum(int x,int y){  //sum=SUM of all A[i][j], where 1<=i<=x and 1<=j<=y
-    int ans=0;
-    int xx=x;
-    while(xx!=0){
-        int yy=y;
-        while(yy!=0) {ans+=BIT[xx][yy];yy-=(yy&-yy);}
-        xx-=(xx&-xx);
-    }
-    return ans;
-}
