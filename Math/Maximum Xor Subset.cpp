377b8ffdc4ddb936361c3d94c7e7c49b587eca3c
Anik Sarker <aniksarkerbuet1997@gmail.com>
2018-09-20 01:52:43 +0600

    Update Maximum Xor Subset.cpp

--- Math/Maximum Xor Subset.cpp
+++ Math/Maximum Xor Subset.cpp
@@ -40,7 +40,7 @@ struct maxxor{
 ll a[MAX], n;
 ll maxxor(){
     int r = 0; ll ret = 0;
-    for(int c = 31; c >= 0; c--){
+    for(int c = 63; c >= 0; c--){
         int idx = -1;
         for(int i = r; i < n && idx < 0; i++)
             if(a[i] >> c & 1) idx = i;
