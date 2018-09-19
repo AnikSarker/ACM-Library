d69a5cabe0ef19d8e1a3b9c3648f7675d57fd486
Double-O <hrdd03@gmail.com>
2018-09-19 12:35:45 +0600

    Update Palindromic Tree.cpp

--- String/Palindromic Tree.cpp
+++ String/Palindromic Tree.cpp
@@ -3,7 +3,9 @@
 
 #include<bits/stdc++.h>
 using namespace std;
-#define MAX 1000005
+#define MAX 300005
+#define INF 1e9
+#define pii pair<int,int>
 
 struct node{
     int Next[26];
@@ -13,13 +15,21 @@ struct node{
     int Count;      //Number of palindromic suffixes of the palindrome represented by current node
     int SuffLink;   //Links such node w such that the palindrome represented by node w is the largest
                     //palindrome which is a proper suffix of the palindrome represented by current node
+
+    //Used for palindrome factorization
+    int Diff;
+    pii SeriesAns;
+    int SerialLink;
+
     void nodeClear(){
         Len = Count = MinPos = Occurrence = SuffLink = 0;
+        Diff = SerialLink = 0; SeriesAns = {INT_MAX,INT_MAX};
         memset(Next, 0, sizeof(Next));
     }
 };
 
 string s;
+pii DP[MAX];
 node Tree[MAX];
 int Suff;           // Points to Current (initially previous) node in palindromic tree
 int Size;           // Size = Total number of node in palindromic tree
@@ -45,34 +55,64 @@ bool AddLetter(int pos){
     Tree[Size].nodeClear();
 
     Suff=Size;
-    Tree[Size].MinPos=pos;
-    Tree[Size].Len=Tree[Cur].Len+2;
-    Tree[Cur].Next[Let]=Size;
+    Tree[Suff].MinPos=pos;
+    Tree[Suff].Len=Tree[Cur].Len+2;
+    Tree[Cur].Next[Let]=Suff;
 
-    if(Tree[Size].Len==1) {Tree[Size].SuffLink=2; Tree[Size].Count=1; return true;}
-
-    while(true){
-        Cur=Tree[Cur].SuffLink;
-        CurLen=Tree[Cur].Len;
-        if(pos-1>=CurLen && s[pos-1-CurLen]==s[pos]){
-            Tree[Size].SuffLink=Tree[Cur].Next[Let];
-            break;
+    if(Tree[Size].Len==1) Tree[Size].SuffLink=2;
+    else{
+        while(true){
+            Cur=Tree[Cur].SuffLink;
+            CurLen=Tree[Cur].Len;
+            if(pos-1>=CurLen && s[pos-1-CurLen]==s[pos]){
+                Tree[Size].SuffLink=Tree[Cur].Next[Let];
+                break;
+            }
         }
     }
-
     Tree[Size].Count=1+Tree[Tree[Size].SuffLink].Count;
+
+    //Update for palindromic factorization Begin
+    Tree[Suff].Diff = Tree[Suff].Len - Tree[Tree[Suff].SuffLink].Len;
+    if(Tree[Suff].Diff == Tree[Tree[Suff].SuffLink].Diff){
+        Tree[Suff].SerialLink = Tree[Tree[Suff].SuffLink].SerialLink;
+    }
+    else Tree[Suff].SerialLink = Suff;
+    //Update for palindromic factorization Ended
+
     return true;
+}
+
+void Update(int pos){
+    DP[pos]={INF,INF};
+    for(int j=Suff; Tree[j].Len > 0;){
+        int slv = Tree[Tree[j].SerialLink].SuffLink;
+        Tree[j].SeriesAns = DP[pos - (Tree[slv].Len + Tree[j].Diff)];
+
+        if(Tree[j].Diff == Tree[Tree[j].SuffLink].Diff){
+            Tree[j].SeriesAns.first = min(Tree[j].SeriesAns.first, Tree[Tree[j].SuffLink].SeriesAns.first);
+            Tree[j].SeriesAns.second = min(Tree[j].SeriesAns.second, Tree[Tree[j].SuffLink].SeriesAns.second);
+        }
+
+        auto val = Tree[j].SeriesAns;
+        DP[pos].first = min(DP[pos].first, val.second + 1);
+        DP[pos].second = min(DP[pos].second, val.first + 1);
+        j = slv;
+    }
 }
 
 void InitTree(){
     Size=2; Suff=2;
+    Tree[1].nodeClear(); Tree[2].nodeClear();
     Tree[1].Len=-1; Tree[1].SuffLink=1;
     Tree[2].Len=0;  Tree[2].SuffLink=1;
+    DP[0] = {INF,0};
 }
 
 int main(){
     //InitTree();
-    //for(int i=0;i<n;i++) AddLetter(i);
+    //cin>>s; int n=s.size(); s="$"+s;
+    //for(int i=1;i<=n;i++) AddLetter(i), Update(i);
     //distinct palindrome count = Size - 2
     //For(i=Size;i>=1;i--) Tree[Tree[i].SuffLink].Occurrence+=Tree[i].Occurrence;
 }
