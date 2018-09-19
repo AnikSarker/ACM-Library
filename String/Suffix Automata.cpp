c9f443890242d9e53bbea0b345d0499459399577
Double-O <hrdd03@gmail.com>
2018-09-19 10:18:20 +0600

    Update Suffix Automata.cpp

--- String/Suffix Automata.cpp
+++ String/Suffix Automata.cpp
@@ -1,32 +1,63 @@
-const int maxn = 1e5+10;
-const int MAX = 127;
-int len[maxn << 1], link[maxn << 1], sz, last;
-int adj[maxn << 1][MAX];
+struct state {
+    int len, link;
+    map<char,int>next;
+    ll pathCount;
 
-void init() {
-	sz = last = 0; 
-	len[0] = 0; link[0] = -1;
-	memset(adj, -1, sizeof adj); 
-	sz++;
+    void Clear(){
+        len = pathCount = 0;
+        link = -1;
+        next.clear();
+    }
+
+};
+
+const int MAXLEN = 100000;
+state st[MAXLEN*2];
+int sz, last;
+
+///root is st[0]
+void sa_init() {
+    sz = last = 0;
+    st[0].Clear();
+    ++sz;
 }
-void extend(int c) { 
-	int cur = sz++, p; 
-	len[cur] = len[last] + 1; 
-	for(p = last; p != -1 && adj[p][c] == -1; p = link[p]) 
-		adj[p][c] = cur;
-	last = cur; 
-	if(p == -1) return void(link[cur] = 0);
 
-	int q = adj[p][c]; 
-	if(len[q] == len[p] + 1) return void(link[cur] = q);
+void sa_extend (char c) {
 
-	int clone = sz++;
-	len[clone] = len[p] + 1; 
-	link[clone] = link[q];
+    int cur = sz++;
+    st[cur].Clear();
 
-	for(int i = 0; i < MAX; i++) 
-		adj[clone][i] = adj[q][i];
-	for(; p != -1 && adj[p][c] == q; p = link[p]) 
-		adj[p][c] = clone;
-	link[q] = link[cur] = clone; 
-} 
+    st[cur].len = st[last].len + 1;
+    int p;
+    for (p=last; p!=-1 && !st[p].next.count(c); p=st[p].link)
+        st[p].next[c] = cur;
+    if (p == -1)
+        st[cur].link = 0;
+    else {
+        int q = st[p].next[c];
+        if (st[p].len + 1 == st[q].len)
+            st[cur].link = q;
+        else {
+            int clone = sz++;
+            st[clone].len = st[p].len + 1;
+            st[clone].next = st[q].next;
+            st[clone].link = st[q].link;
+            for (; p!=-1 && st[p].next[c]==q; p=st[p].link)
+                st[p].next[c] = clone;
+            st[q].link = st[cur].link = clone;
+        }
+    }
+    last = cur;
+}
+
+ll pathCalc(int pos)
+{
+    if(st[pos].pathCount != 0) return st[pos].pathCount;
+    st[pos].pathCount = 1;
+    for(auto to : st[pos].next){
+        ll re = 0;
+        re = pathCalc(to.second);
+        st[pos].pathCount += re;
+    }
+    return st[pos].pathCount;
+}
