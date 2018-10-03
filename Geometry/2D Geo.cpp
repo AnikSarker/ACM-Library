ac1da615cd3cb4ee36ce4929e538c56cf0ae0371
Anik Sarker <aniksarkerbuet1997@gmail.com>
2018-10-03 12:37:42 +0600

    Update 2D Geo.cpp

--- Geometry/2D Geo.cpp
+++ Geometry/2D Geo.cpp
@@ -64,21 +64,6 @@ struct Circle {
     double sector(double alpha) {return r * r * 0.5 * (alpha - sin(alpha));}
 };
 
-struct Star{
-    int n;    // number of side of the star
-    double r; // radius of the circum-circle
-    Star(int n,double r) {this->n=n; this->r=r;}
-
-    double getArea(){
-        double theta=pi/n;
-        double s=2*r*sin(theta);
-        double R=0.5*s/tan(theta);
-        double a=0.5*n*s*R;
-        double a2=0.25*s*s/tan(1.5*theta);
-        return a-n*a2;
-    }
-};
-
 namespace Punctual {
     double getDistance (Point a, Point b) { double x=a.x-b.x, y=a.y-b.y; return sqrt(x*x + y*y); }
 };
@@ -437,5 +422,20 @@ namespace Circular {
         double c = getLength(p1 - p2);
         Point p = (p1 * a + p2 * b + p3 * c) / (a + b + c);
         return Circle(p, getDistanceToLine(p, p1, p2));
+    }
+};
+
+struct Star{
+    int n;    // number of side of the star
+    double r; // radius of the circum-circle
+    Star(int n,double r) {this->n=n; this->r=r;}
+
+    double getArea(){
+        double theta=pi/n;
+        double s=2*r*sin(theta);
+        double R=0.5*s/tan(theta);
+        double a=0.5*n*s*R;
+        double a2=0.25*s*s/tan(1.5*theta);
+        return a-n*a2;
     }
 };
