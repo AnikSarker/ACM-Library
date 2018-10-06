51d42817bb19fc6896011dacb1fb9ba63f7f27ce
Saad Muhammed Junayed <sm.junayed71@gmail.com>
2018-10-06 15:57:34 +0600

    Update Saad Templates.cpp

--- Graph/Saad Templates.cpp
+++ Graph/Saad Templates.cpp
@@ -27,7 +27,139 @@ ll stoer_wagner() {
     }
     return ans ;
 }
-*in NTU , O(NM)
+*Stoer Wagner , O(NM*logN)
+	
+const int inf=1000000000;
+// should be larger than max.possible mincut
+#define MAXN 400
+#define PB push_back
+typedef pair<int,int> pii;
+#define S second
+#define F first
+struct DisjointSet {
+    int p[MAXN];
+    DisjointSet(){
+
+    }
+    DisjointSet(int n) {
+        iota(p,p+n,0);
+    }
+    void init(int n){
+        iota(p,p+n,0);
+    }
+    int getpar(int u) {
+        if(u==p[u])return u;
+        return p[u]=getpar(p[u]);
+    }
+    void merge(int u,int v) {
+        u=getpar(u);
+        v=getpar(v);
+        if(u!=v){
+            p[u]=v;
+        }
+    }
+};
+class StoerWagner {
+  public:
+    int n,mc; // node id in [0,n-1]
+    vector<int> adj[MAXN];
+    int cost[MAXN][MAXN];
+    int cs[MAXN];
+    bool merged[MAXN],sel[MAXN];
+    DisjointSet djs;
+    vector<int> cut;
+    //- 8<
+    StoerWagner(int _n):n(_n),mc(inf) {
+        djs.init(n);
+        for(int i=0; i<n; i++)
+            merged[i]=0;
+        for(int i=0; i<n; i++)
+            for(int j=0; j<n; j++)
+                cost[i][j]=cost[j][i]=0;
+    }
+    void init(int _n){
+        n=_n;
+        mc=inf;
+        cut.clear();
+        djs.init(n);
+        for(int i=0; i<n; i++)
+            merged[i]=0,sel[i]=0,cs[i]=0,adj[i].clear();
+        for(int i=0; i<n; i++)
+            for(int j=0; j<n; j++)
+                cost[i][j]=cost[j][i]=0;
+    }
+
+    void append(int v,int u,int c) {
+        if(v==u) return;
+        if(!cost[v][u]&&c) {
+            adj[v].PB(u);
+            adj[u].PB(v);
+        }
+        cost[v][u]+=c;
+        cost[u][v]+=c;
+    }
+    void merge(int v,int u) {
+        merged[u]=1;
+        for(int i=0; i<n; i++)
+            append(v,i,cost[u][i]);
+// include only if cut is explicitly needed
+       djs.merge(v,u);
+//
+
+    }
+
+    void phase() {
+        priority_queue<pii> pq;
+        for(int v=0; v<n; v++) {
+            if(merged[v]) continue;
+            cs[v]=0;
+            sel[v]=0;
+            pq.push({0,v});
+        }
+        int v,s,pv;
+
+        while(pq.size()) {
+            if(cs[pq.top().S]>pq.top().F) {
+                pq.pop();
+                continue;
+            }
+            pv=v;
+            v=pq.top().S;
+            s=pq.top().F;
+            pq.pop();
+            sel[v]=1;
+            for(int i=0; i<adj[v].size(); i++) {
+                int u=adj[v][i];
+                if(merged[u]||sel[u]) continue;
+                cs[u]+=cost[v][u];
+                pq.push({cs[u],u});
+            }
+        }
+
+        if(s<mc) {
+            mc=s;
+            //include only if cut is explicitly needed
+            cut.clear();
+            for(int i=0; i<n; i++)
+                if(djs.getpar(i)==djs.getpar(v)) cut.PB(i);
+        }
+        merge(v,pv);
+    }
+    int mincut() {
+        if(mc==inf)
+            for(int t=0; t<n-1; t++)
+                phase();
+
+        return mc;
+    }
+    vector<int> getcut() { // return one side of the cut
+        mincut();
+        return cut;
+    }
+};
+
+
+	
 
 Max Flow (Dinic)
 
