fe9c8c620314e4980ea1d16b9c068d30e827f303
Anik Sarker <aniksarkerbuet1997@gmail.com>
2018-10-12 02:57:38 +0600

    Update HashTable.cpp

--- DS/HashTable.cpp
+++ DS/HashTable.cpp
@@ -1,6 +1,17 @@
+#include <bits/stdc++.h.>
 #include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
 using namespace __gnu_pbds;
 
 const int RANDOM = chrono::high_resolution_clock::now().time_since_epoch().count();
-struct chash {int operator() (string x) {return hash<int>{}(x[0] ^ RANDOM);}};
-gp_hash_table<string, int, chash> table;
+unsigned hash_f(unsigned x) {
+    x = ((x >> 16) ^ x) * 0x45d9f3b;
+    x = ((x >> 16) ^ x) * 0x45d9f3b;
+    x = (x >> 16) ^ x;
+    return x;
+}
+
+unsigned hash_combine(unsigned a, unsigned b) { return a * 31 + b; }
+struct chash {int operator()(int x) const { return hash_f(x); }};
+typedef gp_hash_table<int, int, chash>gp;
+gp table;
