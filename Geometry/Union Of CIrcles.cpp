bd6a3a887262381d88b2d00b5fe251b99d3edda0
Anik Sarker <aniksarkerbuet1997@gmail.com>
2018-10-12 08:31:20 +0600

    Update Union Of CIrcles.cpp

--- Geometry/Union Of CIrcles.cpp
+++ Geometry/Union Of CIrcles.cpp
@@ -1,84 +1,5 @@
 // UVa 12056
-#include <bits/stdc++.h>
-using namespace std;
-
-const double EPS = 1e-8;
-const double PI  = acos(-1.0);
-const double TAU = 2.0 * PI;
-const double INF = 1e99;
-
-int sig(double x) {return x < -EPS ? -1 : x > EPS ? 1 : 0;}
-template<class T> T pow2(T x) {return x * x;}
-
-
-class Vector {
-public:
-    double x, y;
-    Vector() {}
-    Vector(double x, double y): x(x), y(y) {}
-
-    Vector operator -() const {return Vector(-x, -y);}
-    Vector operator +(const Vector &v) const {return Vector(x+v.x, y+v.y);}
-    Vector operator -(const Vector &v) const {return Vector(x-v.x, y-v.y);}
-    Vector operator *(const double &s) const {return Vector(x * s, y * s);}
-    Vector operator /(const double &s) const {return Vector(x / s, y / s);}
-
-    double operator *(const Vector &v) const {return x*v.x + y*v.y;}
-    double operator ^(const Vector &v) const {return x*v.y - y*v.x;}
-
-    // rotate vector (Right/Left hand)
-    Vector  R(double co, double si) {return Vector(x*co-y*si, y*co+x*si);}
-    Vector  L(double co, double si) {return Vector(x*co+y*si, y*co-x*si);}
-    Vector  R(double th) {return R(cos(th), sin(th));}
-    Vector  L(double th) {return L(cos(th), sin(th));}
-
-    double len2() {return x*x + y*y;}
-    double len()  {return sqrt(len2());}
-    double ang()  {return atan2(y, x);}  // angle of vector
-    Vector e(double s = 1.0) {return *this / len() * s;}
-};
-typedef Vector Point;
-
-
-class Line {
-public:
-    Point a, b;
-    Line() {}
-    Line(Point a, Point b): a(a), b(b) {}
-};
-
-class Circle {
-public:
-    Point o;
-    double r;
-    Circle() {}
-    Circle(Point o, double r): o(o), r(r) {}
-
-    // interior          (d < R - r)         ----> -2
-    // interior tangents (d = R - r)         ----> -1
-    // concentric        (d = 0)
-    // secants           (R - r < d < R + r) ---->  0
-    // exterior tangents (d = R + r)         ---->  1
-    // exterior          (d > R + r)         ---->  2
-    int posi(Circle c) {
-        double d = (o - c.o).len();
-        int in = sig(d - fabs(r - c.r)), ex = sig(d - (r + c.r));
-        return in<0 ? -2 : in==0? -1 : ex==0 ? 1 : ex>0? 2 : 0;
-    }
-
-    // IMPORTANT: Ensure this->posi(c) = 0
-    // chord(弦) of two circle
-    // two points is ccw in *this circle
-    Line chord(Circle c) {
-        Vector v = c.o - o;
-        double co = (pow2(r) + v.len2() - pow2(c.r)) / (2 * r * v.len());
-        double si = sqrt(fabs(1.0 - pow2(co)));
-        return Line(v.L(co, si).e(r) + o, v.R(co, si).e(r) + o);
-    }
-};
-
-
-// -PI <= th <= PI
+// -pi <= th <= pi
 struct Range {
     double t;
     int evt;
@@ -87,78 +8,61 @@ struct Range {
     Range(double t, int evt, Point p) : t(t), evt(evt), p(p) {}
 
     bool operator <(const Range &s) const {
-        return sig(t - s.t) < 0 || (sig(t - s.t) == 0 && evt > s.evt);
+        return dcmp(t - s.t) < 0 || (dcmp(t - s.t) == 0 && evt > s.evt);
     }
 };
 
-const int MAX = 1000 + 10;
+const int MAX = 1005;
 Circle C[MAX];
-Range R[MAX*2];
+vector<Range>R;
 
-// sort circle with desending of radii
 bool cmp_r(const Circle &a, const Circle &b) {return a.r > b.r;}
-// Area of circular segment(弓形)
-double segment_area(double r, double t) {
-    return pow2(r) * (t - sin(t)) / 2;
-}
-double union_circle(Circle C[], int &n)
-{
+double AreaUnionCircle(Circle C[], int &n){
+    using namespace Vectorial;
+    using namespace Circular;
+
     sort(C, C + n, cmp_r);
     int k = 0;
-    for (int i = 0; i < n; i++) {
-        if (sig(C[i].r) == 0) break;
+    for (int i = 0; i < n; i++){
+        if (dcmp(C[i].r) == 0) break;
         int j = 0;
-        for (j = 0; j < k; j++)
-            if (C[i].posi(C[j]) < 0 || !sig((C[i].o - C[j].o).len()))
-                break;
-        if (j == k)
-            C[k++] = C[i];
+        for (j = 0; j < k; j++) //interior or concentric
+            if(getPos(C[i],C[j]) < 0 || !dcmp(getLength(C[i].o - C[j].o))) break;
+        if (j == k) C[k++] = C[i];
     }
     n = k;
 
     double ans = 0;
-    for (int i = 0; i < n; ++ i)
-    {
-        Point mpi = Point(- C[i].r, 0.0) + C[i].o;
-        int nc = 0, rcnt = 0;
-        R[rcnt++] = Range(-PI,  1, mpi);
-        R[rcnt++] = Range( PI, -1, mpi);
-        for (int j = 0; j < n; ++ j)
-        {
-            if (j == i || C[i].posi(C[j])) continue;
+    for (int i = 0; i < n; ++ i){
+        int nc = 0;
+        R.clear();
+        Point mpi = Point(- C[i].r, 0) + C[i].o;
+        R.push_back(Range(-pi,  1, mpi));
+        R.push_back(Range( pi, -1, mpi));
 
-            Line l = C[i].chord(C[j]);
-            double jR = (l.a - C[i].o).ang(), jL = (l.b - C[i].o).ang();
+        for (int j = 0; j < n; ++ j){
+            if (j == i) continue;
 
-            if (sig(jR - jL) > 0) ++ nc;
-            R[rcnt++] = Range(jR,  1, l.a);
-            R[rcnt++] = Range(jL, -1, l.b);
+            vector<Point>sol;
+            int ret = getCircleCircleIntersection(C[i],C[j],sol);
+            if(ret<2) continue;
+
+            Point a = sol[0]; Point b = sol[1];
+            double jR = getAngle(a - C[i].o), jL = getAngle(b - C[i].o);
+            if (dcmp(jR - jL) > 0) ++ nc;
+            R.push_back(Range(jR,  1, a));
+            R.push_back(Range(jL, -1, b));
         }
-        sort(R, R + rcnt);
+        sort(R.begin(),R.end());
 
-        double pj = - PI;
+        double pj = - pi;
         Point  pp = mpi;
-        for(int j = 0; j < rcnt; ++ j)
-        {
-            nc += R[j].evt;
-            if((nc == 2 && R[j].evt > 0) || nc == 0)
-                ans += segment_area(C[i].r, R[j].t - pj) + (pp ^ R[j].p) / 2;
-            pj = R[j].t; pp = R[j].p;
+        for(Range rng : R){
+            nc += rng.evt;
+            if((nc == 2 && rng.evt > 0) || nc == 0)
+                ans += C[i].sector(rng.t - pj) + getCross(pp,rng.p) / 2;
+            pj = rng.t; pp = rng.p;
         }
     }
     return ans;
 }
-
-int main(){
-    int n;
-    while (scanf("%d", &n) != EOF) {
-        if(n==0) break;
-        for (int i = 0; i < n; i++)
-            scanf("%lf%lf%lf", &C[i].o.x, &C[i].o.y, &C[i].r);
-
-        double ans = union_circle(C, n);
-        printf("%.3lf\n", ans);
-    }
-    return 0;
-}
-
