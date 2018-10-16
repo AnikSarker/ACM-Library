34057539032caca6d974d290643530421a59166e
Anik Sarker <aniksarkerbuet1997@gmail.com>
2018-10-17 04:51:46 +0600

    Update Segment Tree (Persistent).cpp

--- DS/Segment Tree (Persistent).cpp
+++ DS/Segment Tree (Persistent).cpp
@@ -4,23 +4,25 @@ struct node{
     node(){val=0;left=NULL;right=NULL;}
     node(int a,node *b,node *c){val=a,left=b,right=c;}
 
-    void build(int lo, int hi) {
-	if(lo==hi) return;
-        left=new node();
-        right=new node();
-        int mid=(lo+hi)/2;
-        left->build(lo,mid);
-        right->build(mid+1,hi);
-    }
-
     node *update(int lo,int hi,int i,int v){
-        if(hi<i || lo>i) return this;
-        if(lo==hi) {node *ret=new node(val,left,right); ret->val+=v; return ret;}
+        node* ret = new node(val,left,right);
+
+        if(lo==hi) {ret->val += v; return ret;}
         int mid=(lo+hi)/2;
-        node *ret=new node();
-        ret->left=left->update(lo,mid,i,v);
-        ret->right=right->update(mid+1,hi,i,v);
-        ret->val=ret->left->val+ret->right-> val;
+
+        if(i<=mid){
+            if(!left) left = new node();
+            ret->left = left->update(lo,mid,i,v);
+        }
+        else{
+            if(!right) right = new node();
+            ret->right = right->update(mid+1,hi,i,v);
+        }
+
+        ret->val=0;
+        if(ret->left)  ret->val += ret->left->val;
+        if(ret->right) ret->val += ret->right-> val;
+
         return ret;
     }
 
@@ -28,19 +30,26 @@ struct node{
         if(hi<i || lo>j) return 0;
         if(i<=lo && hi<=j) return val;
         int mid=(lo+hi)/2;
-        return left->query(lo,mid,i,j)+right->query(mid+1,hi,i,j);
+
+        int ret = 0;
+        if(left) ret += left->query(lo,mid,i,j);
+        if(right) ret += right->query(mid+1,hi,i,j);
+        return ret;
     }
 };
-int Size;
-node* root[MAX];
 
 //Searching kth minimum element in sorted order
-int Search(node *a,node *b,node* c,node* d,int l,int r,int k){
-    if(l==r) return l;
-    int Count=a->left->val+b->left->val-c->left->val-d->left->val;
-    int mid=(l+r)/2;
-    if(Count>=k) return Search(a->left,b->left,c->left,d->left,l,mid,k);
-    else return Search(a->right,b->right,c->right,d->right,mid+1,r,k-Count);
+inline int Val(node* x)    {return x ? x->val : 0;}
+inline node* Left(node* x)   {return x ? x->left : NULL;}
+inline node* Right(node* x)  {return x ? x->right : NULL;}
+
+int Search(node *a,node *b,node* c,node* d,int l,int r,int k) {
+  if(l==r) return l;
+  int Count=Val(Left(a))+Val(Left(b))-Val(Left(c))-Val(Left(d));
+
+  int mid=(l+r)/2;
+  if(Count>=k) return Search(Left(a),Left(b),Left(c),Left(d),l,mid,k);
+  else return Search(Right(a),Right(b),Right(c),Right(d),mid+1,r,k-Count);
 }
 
 void dfs(int u,int p,int d){
