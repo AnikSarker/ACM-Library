29dc7265800025908675ee55ae63e3c9912d6c62
Anik Sarker <aniksarkerbuet1997@gmail.com>
2018-11-01 11:21:07 +0600

    Update CHT (Dynamic).cpp

--- DP/CHT (Dynamic).cpp
+++ DP/CHT (Dynamic).cpp
@@ -3,41 +3,70 @@
 using namespace std;
 #define ll long long int
 
-const ll is_query = -(1LL<<62);
-const ll INF = 1LL << 60;
-struct Line{ // y=m*x+b
-    ll m,b;
-    mutable function<const Line*()> succ;
-    bool operator<(const Line& rhs) const{
-        if (rhs.b!=is_query) return m<rhs.m;
-        const Line* s = succ();
-        if (!s) return 0;
-        ll x = rhs.m;
-        return b - s->b < (s->m - m) * x;
-    }
-};
+ll inf = 9e18 + 5;
 
-struct HullDynamic : public multiset<Line> {// will maintain upper hull for maximum
-    bool bad(iterator y) {
-        auto z = next(y);
-        if (y == begin()) {
-            if (z == end()) return 0;
-            return y->m == z->m && y->b <= z->b;
-        }
-        auto x = prev(y);
-        if (z == end()) return y->m == x->m && y->b <= x->b;
-        return 1.0*(x->b - y->b)*(z->m - y->m) > 1.0 * (y->b - z->b)*(y->m - x->m);
-    }
-    void Add(ll m, ll b) {
-        auto y = insert({ m, b });
-        y->succ = [=] { return next(y) == end() ? 0 : &*next(y); };
-        if (bad(y)) { erase(y); return; }
-        while (next(y) != end() && bad(next(y))) erase(next(y));
-        while (y != begin() && bad(prev(y))) erase(prev(y));
-    }
-    ll Query(ll x) {
-        auto l = lower_bound((Line){x,is_query});
-        if (l == end()) return -INF;
-        return l->m * x + l->b;
-    }
+struct HullDynamic{
+	struct line {
+		ll m, b; ld x;
+		ll val; bool isQuery;
+		line(ll _m = 0, ll _b = 0) :
+			m(_m), b(_b), val(0), x(-inf), isQuery(false) {}
+
+		ll eval(ll x) const { return m * x + b;	}
+		bool parallel(const line &l) const { return m == l.m; }
+		ld intersect(const line &l) const {
+			return parallel(l) ? inf : 1.0 * (l.b - b) / (m - l.m);
+		}
+		bool operator < (const line &l) const {
+			if(l.isQuery) return x < l.val;
+			else return m < l.m;
+		}
+	};
+
+	set<line> hull;
+	typedef set<line> :: iterator iter;
+
+	bool cPrev(iter it) { return it != hull.begin(); }
+	bool cNext(iter it) { return it != hull.end() && next(it) != hull.end(); }
+
+	bool bad(const line &l1, const line &l2, const line &l3) {
+		return l1.intersect(l3) <= l1.intersect(l2);
+	}
+	bool bad(iter it) {
+		return cPrev(it) && cNext(it) && bad(*prev(it), *it, *next(it));
+	}
+
+	iter update(iter it) {
+		if(!cPrev(it)) return it;
+		ld x = it -> intersect(*prev(it));
+		line tmp(*it); tmp.x = x;
+		it = hull.erase(it);
+		return hull.insert(it, tmp);
+	}
+
+	void Add(ll m, ll b) {
+		line l(m, b);
+		iter it = hull.lower_bound(l);
+		if(it != hull.end() && l.parallel(*it)) {
+			if(it -> b < b) it = hull.erase(it);
+			else return;
+		}
+
+		it = hull.insert(it, l);
+		if(bad(it)) return (void) hull.erase(it);
+
+		while(cPrev(it) && bad(prev(it))) hull.erase(prev(it));
+		while(cNext(it) && bad(next(it))) hull.erase(next(it));
+
+		it = update(it);
+		if(cPrev(it)) update(prev(it));
+		if(cNext(it)) update(next(it));
+	}
+
+	ll Query(ll x) const {
+		if(hull.empty()) return -inf;
+		line q; q.val = x, q.isQuery = 1;
+		iter it = --hull.lower_bound(q);
+		return it -> eval(x);
+	}
 };
