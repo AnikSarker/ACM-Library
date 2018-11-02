1a19316397b3d71fc5cd2cb9308fd3e7c423bec8
Anik Sarker <aniksarkerbuet1997@gmail.com>
2018-11-03 04:38:38 +0600

    Update Knight Distance.cpp

--- AdHoc/Knight Distance.cpp
+++ AdHoc/Knight Distance.cpp
@@ -2,67 +2,14 @@
 using namespace std;
 #define ll long long int
 
-//O(1)
-struct point{
-    ll x, y;
-    bool operator < (const point &p) const {return x == p.x ? y < p.y : x < p.x;}
-};
+// Minimum number of knight moves from (x,y) to
+// (0,0) in non-negative infinite chessboard
 
-ll kdist(point p, point q){
-    ll dx = abs(p.x - q.x);
-    ll dy = abs(p.y - q.y);
-    ll cnt = max({(dx+1)/2, (dy+1)/2, (dx+dy+2)/3});
-    while((cnt%2) != (dx+dy)%2) cnt++;
-    if(dx == 1 && !dy) return 3;
-    if(dy == 1 && !dx) return 3;
-    if(dx == dy && dx == 2) return 4;
+ll knight_move(ll x, ll y){
+    ll cnt = max({(x+1)/2, (y+1)/2, (x+y+2)/3});
+    while((cnt%2) != (x+y)%2) cnt++;
+    if(x == 1 && !y) return 3;
+    if(y == 1 && !x) return 3;
+    if(x == y && x == 2) return 4;
     return cnt;
-}
-
-// O(log Max)
-const int KN = 101;
-ll dk[KN][KN];
-int dx[] = {-1, -1, 1, 1, -2, -2, 2, 2};
-int dy[] = {-2, 2, -2, 2, -1, 1, -1, 1};
-
-void precalc(){
-    int x, y, x1, y1, i;
-    queue< int > Q;
-    memset(dk, 0x3f, sizeof dk);
-    x = y = (KN >> 1);
-    dk[x][y] = 0;
-    Q.push(x);
-    Q.push(y);
-    while(!Q.empty()){
-        x = Q.front();
-        Q.pop();
-        y = Q.front();
-        Q.pop();
-        for(i = 0; i < 8; i++){
-            x1 = x + dx[i], y1 = y + dy[i];
-            if(0 <= x1 && x1 < KN && 0 <= y1 && y1 < KN){
-                if(dk[x1][y1] > dk[x][y] + 1){
-                    dk[x1][y1] = dk[x][y] + 1;
-                    Q.push(x1);
-                    Q.push(y1);
-                }
-            }
-        }
-    }
-}
-
-// Returns knight distance between (0,0) -> (x,y)
-ll knight(ll x,ll y){
-    ll step, res = 0;
-    if(x < y) swap(x, y);
-    while((x<<1) > KN){
-        step = x / 2 / 2;
-        res += step;
-        x -= step * 2;
-        y -= step;
-        if(y < 0) y = ((y % 2) + 2) % 2;
-        if(x < y) swap(x, y);
-    }
-    res += dk[x+(KN>>1)][y+(KN>>1)];
-    return res;
 }
