9936a0defaadf4ab360a14a1972b47c0f5a1a8d3
Anik Sarker <aniksarkerbuet1997@gmail.com>
2018-11-04 04:48:04 +0600

    Update Suffix Automata.cpp

--- String/Suffix Automata.cpp
+++ String/Suffix Automata.cpp
@@ -1,46 +1,46 @@
-struct state {
-    int len, link;
-    map<char,int>next;
+struct State{
+    int Len, Link;
+    map<char,int>Next;
     ll pathCount;
-    void Clear(){len = pathCount = 0; link= -1; next.clear();}
+    void Clear() {Len = pathCount = 0; Link= -1; Next.clear();}
 };
 
 const int MAXLEN = 100000;
-state st[MAXLEN*2];
-int sz, last;
+State St[MAXLEN*2];
+int Size, Last;
 
-///root is st[0]
-void sa_init() {sz = last = 0; st[0].Clear();}
+// root is St[0]
+void SA_Init() {Size = Last = 0; St[0].Clear();}
 
-void sa_extend (char c) {
-    int cur = ++sz;
-    st[cur].Clear();
+void SA_Extend(char c){
+    int cur = ++Size;
+    St[cur].Clear();
 
-    st[cur].len = st[last].len + 1;
+    St[cur].Len = St[Last].Len + 1;
     int p;
-    for(p=last; p!=-1 && !st[p].next.count(c); p=st[p].link)
-        st[p].next[c] = cur;
-    
-    if(p == -1) st[cur].link = 0;
+    for(p=Last; p!=-1 && !St[p].Next.count(c); p=St[p].Link)
+        St[p].Next[c] = cur;
+
+    if(p == -1) St[cur].Link = 0;
     else{
-        int q = st[p].next[c];
-        if (st[p].len + 1 == st[q].len) st[cur].link = q;
+        int q = St[p].Next[c];
+        if(St[p].Len + 1 == St[q].Len) St[cur].Link = q;
         else{
-            int clone = ++sz;
-            st[clone].len = st[p].len + 1;
-            st[clone].next = st[q].next;
-            st[clone].link = st[q].link;
-            for (; p!=-1 && st[p].next[c]==q; p=st[p].link)
-                st[p].next[c] = clone;
-            st[q].link = st[cur].link = clone;
+            int clone = ++Size;
+            St[clone].Len = St[p].Len + 1;
+            St[clone].Next = St[q].Next;
+            St[clone].Link = St[q].Link;
+            for(; p!=-1 && St[p].Next[c]==q; p=St[p].Link)
+                St[p].Next[c] = clone;
+            St[q].Link = St[cur].Link = clone;
         }
     }
-    last = cur;
+    Last = cur;
 }
 
 ll pathCalc(int pos){
-    if(st[pos].pathCount != 0) return st[pos].pathCount;
+    if(St[pos].pathCount != 0) return St[pos].pathCount;
     ll re = 1;
-    for(auto to : st[pos].next) re += pathCalc(to.second);
-    return st[pos].pathCount = re;
+    for(auto to : St[pos].Next) re += pathCalc(to.second);
+    return St[pos].pathCount = re;
 }
