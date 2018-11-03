8ad81f778f7000dac167f2c40f4184bfcbd33a67
Anik Sarker <aniksarkerbuet1997@gmail.com>
2018-11-04 04:37:42 +0600

    Update Minimum Lexicographical Rotation.cpp

--- String/Minimum Lexicographical Rotation.cpp
+++ String/Minimum Lexicographical Rotation.cpp
@@ -1,9 +1,10 @@
-///Taken from Rezwan Arefin
-int minimumExpression(string s) { s = s + s; 
+int minimumExpression(string s) {
+	s = s + s; 
 	int i = 0, j = 1, k = 0, len = s.size(); 
 	while(i + k < len && j + k < len) {
 		if(s[i + k] == s[j + k]) k++;
 		else if(s[i + k] < s[j + k]) { j = max(j + k + 1, i + 1); k = 0; }
 		else { i = max(i + k + 1, j + 1); k = 0; }
-	} return min(i, j);
-} 
+	}
+	return min(i, j);
+}
