3f3d8ecb1947fc1bc13910b5c9fc9d15249b0e7d
Saad Muhammed Junayed <sm.junayed71@gmail.com>
2018-11-03 20:55:02 +0600

    Update Dinic.cpp

--- Graph/Flow/Dinic.cpp
+++ Graph/Flow/Dinic.cpp
@@ -1,7 +1,11 @@
-const int MAXN = 500+5 ;
+const int MAXN = 2000+5 ;
 const int INF = 1000000000 ;
+typedef double T;
+const T eps = 1e-9;
 struct edge {
-    int a, b, cap, flow, yo, x, y;
+    int a, b;
+    T cap,flow;
+    int yo, x, y;
 };
 struct Dinic {
     int s,t,d[MAXN], ptr[MAXN] ;
@@ -12,9 +16,9 @@ struct Dinic {
         memset(d,0,sizeof(d));
         for(int i = 0; i < MAXN ; i++)g[i].clear();
     }
-    void addEdge(int a,int b,int cap, int x = -1, int y= -1) {
-        edge e1 = { a, b, cap, 0, 1, x, y } ;
-        edge e2 = { b, a, 0, 0, 0, x, y } ;
+    void addEdge(int a,int b,T cap, int x = -1, int y= -1) {
+        edge e1 = { a, b, cap, 0.0, 1, x, y } ;
+        edge e2 = { b, a, 0.0, 0.0, 0, x, y } ;
         g[a].push_back((int)e.size());
         e.push_back(e1);
         g[b].push_back((int)e.size());
@@ -31,6 +35,7 @@ struct Dinic {
             for(int i=0; i<g[u].size(); i++) {
                 int id=g[u][i];
                 int v=e[id].b;
+              //  printf("%d %d %0.3lf %0.3lf\n",u,v,e[id].cap,e[id].flow);
                 if(d[v]==-1&&e[id].flow<e[id].cap) {
                     Q.push(v) ;
                     d[v]=d[u]+1 ;
@@ -39,15 +44,16 @@ struct Dinic {
         }
         return d[t]!=-1 ;
     }
-    int dfs(int u,int flow) {
-        if (!flow)  return 0 ;
+    T dfs(int u,T flow) {
+        if (flow<=eps)  return 0 ;
         if ( u==t )  return flow ;
         for(int& i = ptr[u] ; i<g[u].size(); i++) {
             int id = g[u][i];
             int v =  e[id].b ;
             if ( d[v] != d[u]+1 )  continue ;
-            int pushed = dfs (v,min (flow,e[id].cap-e[id].flow)) ;
-            if (pushed) {
+            T pushed = dfs (v,min (flow,e[id].cap-e[id].flow)) ;
+            //cout << "pushed " << pushed << endl;
+            if (pushed>eps) {
                 e [id].flow+=pushed ;
                 e [id^1].flow-=pushed ;
                 return pushed ;
@@ -55,12 +61,16 @@ struct Dinic {
         }
         return 0 ;
     }
-    int dinic() {
-        int flow = 0 ;
+    T dinic() {
+        T flow = 0 ;
         while(true) {
             if(!bfs())  break ;
             memset(ptr, 0, sizeof(ptr)) ;
-            while ( int pushed = dfs(s,INF ) )  flow += pushed ;
+            while (true){
+                T pushed = dfs(s,INF );
+                if(pushed<=eps)break;
+                flow += pushed ;
+            }
         }
         return flow ;
     }
