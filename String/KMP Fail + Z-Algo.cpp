9a295e98863689332d66b5973917f1fa635306c4
Double-O <hrdd03@gmail.com>
2018-11-04 21:40:57 +0600

    Update KMP Fail + Z-Algo.cpp

--- String/KMP Fail + Z-Algo.cpp
+++ String/KMP Fail + Z-Algo.cpp
@@ -10,6 +10,29 @@ void build_failure(string s){
         fail[i]=j;
     }
 }
+// s er modhe t khujo
+int SubstringSearch(string s, string t)
+{
+    bool flg = false;
+
+    for(int i = 0, j = 0; i <= s.size(); ){
+            if(j == t.size()){
+                flg = true;
+                break;
+            }
+            if(i == s.size()) break;
+            if(s[i] == t[j]){
+                i++, j++;
+                continue;
+            }
+            else{
+                if(j != 0) j = fail[j - 1];
+                else i++;
+            }
+        }
+        if(flg) return 1;
+        else return 0;
+}
 
 //0-indexed string s[0...n-1]
 //z[i] = maximum x such that s[1....x] matches with s[i..i+x-1]
