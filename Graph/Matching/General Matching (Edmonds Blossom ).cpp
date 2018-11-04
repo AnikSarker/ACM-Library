728fd35b7c13de06c3b29e667a93e6531760eb5e
Saad Muhammed Junayed <sm.junayed71@gmail.com>
2018-11-04 22:07:57 +0600

    Update General Matching (Edmonds Blossom ).cpp

--- Graph/Matching/General Matching (Edmonds Blossom ).cpp
+++ Graph/Matching/General Matching (Edmonds Blossom ).cpp
@@ -1,90 +1,106 @@
 //complexity EV^2,0-indexed
-const int M=500;
-struct struct_edge {
-    int v;
-    struct_edge* n;
-};
-typedef struct_edge* edge;
-struct_edge pool[M*M*2];
-edge top=pool,adj[M];
-int V,E,match[M],qh,qt,q[M],father[M],base[M];
-bool inq[M],inb[M],ed[M][M];
-void add_edge(int u,int v) {
-    top->v=v,top->n=adj[u],adj[u]=top++;
-    top->v=u,top->n=adj[v],adj[v]=top++;
+const int maxn=250;
+struct Edge{int to,next;}edge[maxn*maxn];
+int Adj[maxn],Size;
+void init(){memset(Adj,-1,sizeof(Adj)); Size=0;}
+void add_edge(int u,int v){
+  edge[Size].to=v; edge[Size].next=Adj[u]; Adj[u]=Size++;
 }
-int LCA(int root,int u,int v) {
-    static bool inp[M];
-    memset(inp,0,sizeof(inp));
-    while(1) {
-        inp[u=base[u]]=true;
-        if (u==root) break;
-        u=father[match[u]];
+int n,Match[maxn];
+bool G[maxn][maxn];
+int Start,Finish,NewBase,Father[maxn],Base[maxn];
+bool InQueue[maxn],InPath[maxn],InBlossom[maxn];
+int Count;
+queue<int> q;
+int FindCommonAncestor(int u,int v){
+  memset(InPath,false,sizeof(InPath));
+  while(true){
+      u=Base[u];InPath[u]=true;
+      if(u==Start) break;
+      u=Father[Match[u]];
     }
-    while(1) {
-        if (inp[v=base[v]]) return v;
-        else v=father[match[v]];
+  while(true){
+      v=Base[v];
+      if(InPath[v]) break;
+      v=Father[Match[v]];
+    }
+  return v;
+}
+void ResetTrace(int u){
+  int v;
+  while(Base[u]!=NewBase){
+      v=Match[u];
+      InBlossom[Base[u]]=InBlossom[Base[v]]=true;
+      u=Father[v];
+      if(Base[u]!=NewBase) Father[u]=v;
     }
 }
-void mark_blossom(int lca,int u) {
-    while (base[u]!=lca) {
-        int v=match[u];
-        inb[base[u]]=inb[base[v]]=true;
-        u=father[v];
-        if (base[u]!=lca) father[u]=v;
-    }
-}
-void blossom_contraction(int s,int u,int v) {
-    int lca=LCA(s,u,v);
-    memset(inb,0,sizeof(inb));
-    mark_blossom(lca,u);
-    mark_blossom(lca,v);
-    if (base[u]!=lca)  father[u]=v;
-    if (base[v]!=lca) father[v]=u;
-    for (int u=0; u<V; u++)
-        if (inb[base[u]]) {
-            base[u]=lca;
-            if (!inq[u]) inq[q[++qt]=u]=true;
+void BlosomContract(int u,int v){
+  NewBase=FindCommonAncestor(u,v);
+  memset(InBlossom,false,sizeof(InBlossom));
+  ResetTrace(u);ResetTrace(v);
+  if(Base[u]!=NewBase) Father[u]=v;
+  if(Base[v]!=NewBase) Father[v]=u;
+  for(int tu=1;tu<=n;tu++){
+      if(InBlossom[Base[tu]]){
+          Base[tu]=NewBase;
+          if(!InQueue[tu]){
+              q.push(tu);
+              InQueue[tu]=true;
+            }
         }
+    }
 }
-int find_augmenting_path(int s) {
-    memset(inq,0,sizeof(inq));
-    memset(father,-1,sizeof(father));
-    for (int i=0; i<V; i++) base[i]=i;
-    inq[q[qh=qt=0]=s]=true;
-    while (qh<=qt) {
-        int u=q[qh++];
-        for (edge e=adj[u]; e; e=e->n) {
-            int v=e->v;
-            if (base[u]!=base[v]&&match[u]!=v)
-                if ((v==s)||(match[v]!=-1 && father[match[v]]!=-1))
-                    blossom_contraction(s,u,v);
-                else if (father[v]==-1) {
-                    father[v]=u;
-                    if (match[v]==-1) return v;
-                    else if (!inq[match[v]])
-                        inq[q[++qt]=match[v]]=true;
+void FindAugmentingPath(){
+  memset(InQueue,false,sizeof(InQueue));
+  memset(Father,0,sizeof(Father));
+  for(int i=1;i<=n;i++)Base[i]=i;
+  while(!q.empty()) q.pop();
+  q.push(Start); InQueue[Start]=true;
+  Finish=0;
+  while(!q.empty()){
+      int u=q.front(); //InQueue[u]=false;
+      q.pop();
+      for(int i=Adj[u];~i;i=edge[i].next){
+          int v=edge[i].to;
+          if((Base[u]!=Base[v])&&Match[u]!=v){
+              if(v==Start||(Match[v]>0&&Father[Match[v]]>0))
+                BlosomContract(u,v);
+              else if(Father[v]==0){
+                  Father[v]=u;
+                  if(Match[v]>0){
+                      q.push(Match[v]);
+                      InQueue[Match[v]]=true;
+                    }
+                  else{
+                      Finish=v;
+                      return ;
+                    }
                 }
+            }
         }
     }
-    return -1;
 }
-int augment_path(int s,int t) {
-    int u=t,v,w;
-    while (u!=-1) {
-        v=father[u];
-        w=match[v];
-        match[v]=u;
-        match[u]=v;
-        u=w;
+void AugmentPath(){
+  int u,v,w;u=Finish;
+  while(u>0){
+      v=Father[u]; w=Match[v];
+      Match[v]=u;Match[u]=v;u=w;
     }
-    return t!=-1;
 }
-int edmonds() {
-    int matchc=0;
-    memset(match,-1,sizeof(match));
-    for(int u=0; u<V; u++)
-        if (match[u]==-1)
-            matchc+=augment_path(u,find_augmenting_path(u));
-    return matchc;
+void Edmonds(){
+  memset(Match,0,sizeof(Match));
+  for(int u=1;u<=n;u++){
+      if(Match[u]==0){
+          Start=u;
+          FindAugmentingPath();
+          if(Finish>0) AugmentPath();
+        }
+    }
+}
+void PrintMatch(){
+  Count=0;
+  for(int i=1;i<=n;i++)if(Match[i])Count++;
+  printf("%d\n",Count);
+  for(int u=1;u<=n;u++)if(u<Match[u])printf("%d %d\n",u,Match[u]);
 }
