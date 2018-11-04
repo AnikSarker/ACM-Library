01a25966cccad0f27748846968468812eda51d7c
Saad Muhammed Junayed <sm.junayed71@gmail.com>
2018-11-04 18:08:34 +0600

    Update EulerWalk.cpp

--- Graph/Miscellaneous/EulerWalk.cpp
+++ Graph/Miscellaneous/EulerWalk.cpp
@@ -4,31 +4,37 @@
  empty list if no cycle/path exists. To get edge indices back, also put it->second in s (and then ret).
  Time: O(E) where E is the number of edges.
  */
-
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+typedef vector<pii> vpi;
+#define sz(x) (int)x.size()
 struct V {
 	vector<pii> outs; // (dest, edge index)
 	int nins = 0;
+	void clr(){nins=0;outs.clear();}
 };
-
-vi euler_walk(vector<V>& nodes, int nedges, int src=0) {
+#define ff first
+#define ss second
+vpi euler_walk(vector<V>& nodes, int nedges, int src=0) {
 	int c = 0;
-	trav(n, nodes) c += abs(n.nins - sz(n.outs));
+	for(auto &n:nodes)c += abs(n.nins - sz(n.outs));
 	if (c > 2) return {};
 	vector<vector<pii>::iterator> its;
-	trav(n, nodes)
+	for(auto &n:nodes)
 		its.push_back(n.outs.begin());
-	vector<bool> eu(nedges);
-	vi ret, s = {src};
+	vector<bool> eu(nedges+7);
+	vpi ret, s = {{src,0}};
 	while(!s.empty()) {
-		int x = s.back();
-		auto& it = its[x], end = nodes[x].outs.end();
+		auto x = s.back();
+		auto& it = its[x.ff], end = nodes[x.ff].outs.end();
 		while(it != end && eu[it->second]) ++it;
 		if(it == end) { ret.push_back(x); s.pop_back(); }
-		else { s.push_back(it->first); eu[it->second] = true; }
+		else { s.push_back(*it); eu[it->second] = true; }
 	}
 	if(sz(ret) != nedges+1)
 		ret.clear(); // No Eulerian cycles/paths.
 	// else, non-cycle if ret.front() != ret.back()
-	reverse(all(ret));
+	reverse(ret.begin(),ret.end());
 	return ret;
 }
+vector<V>nodes(maxn);
