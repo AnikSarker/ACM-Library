4269db6c7bfa3d2942a27229247ebf4c881cfbbc
Saad Muhammed Junayed <sm.junayed71@gmail.com>
2018-11-04 15:22:37 +0600

    Update BellmanFord.cpp

--- Graph/shortestPath/BellmanFord.cpp
+++ Graph/shortestPath/BellmanFord.cpp
@@ -1,11 +1,21 @@
-void BellmanFord() {
-    for(ll i=1; i<=n; i++)for(ll j=0; j<m; j++)
+typedef double ll;
+const int maxn = 105;
+const int maxm = 10005;
+const ll inf = 1e9;
+ll d[maxn],w[maxm];
+int u[maxm],v[maxm],n,m;
+bool BellmanFord(){//1-indexed
+    for(int i=1;i<=n;i++)d[i]=inf;
+    d[1]=0;
+    for(int i=1; i<=n; i++)
+        for(int j=0; j<m; j++)
             if(d[u[j]]+w[j] < d[v[j]])
-                d[v[j]]=d[u[j]]+w[j],ck[v[j]]=cs;
+                d[v[j]]=d[u[j]]+w[j];
     bool negCycle = false;
-    for(ll j=0; j<m; j++)
+    for(int j=0; j<m; j++)
         if(d[u[j]]+w[j] < d[v[j]]) {
             negCycle=true;
             break;
         }
+    return negCycle;
 }
