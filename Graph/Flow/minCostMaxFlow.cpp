14ae1a003bfac8146b2a6aef792f9881f430e482
Saad Muhammed Junayed <sm.junayed71@gmail.com>
2018-11-05 14:11:11 +0600

    Update minCostMaxFlow.cpp

--- Graph/Flow/minCostMaxFlow.cpp
+++ Graph/Flow/minCostMaxFlow.cpp
@@ -1,69 +1,110 @@
-typedef long long ll;
-const ll maxn = 20100;
-const ll mod = 1e9+7;
-const ll INF = 1e12;
+typedef long long T1;//for cost
+typedef long long T2;//for flow
+const int maxn = 20100;
+const T1 INF = 1e12;
+const T2 inf = 1e12;
+const T1 eps = 0;
 struct Edge {
-    ll from, to, cap, flow;
-    ll cost;
+    int from, to;
+    T2 cap, flow;
+    T1 cost;
 };
 struct MCMF {//0-indexed
-    ll n, m, s, t;
+    int n, m, s, t;
     vector<Edge> edges;
-    vector<ll> G[maxn];
-    ll inq[maxn], d[maxn], p[maxn], a[maxn];
-    void init(ll n) {
+    vector<int> G[maxn];
+    int p[maxn],inq[maxn];
+    T1 d[maxn];
+    T2 a[maxn];
+    void init(int n) {
         this->n = n;
-        for(ll i = 0; i < n; i++) G[i].clear();
+        for(int i = 0; i < n; i++) G[i].clear();
         edges.clear();
     }
-    void AddEdge(ll from, ll to, ll cap, ll cost) {
+    void AddEdge(int from,int to,T2 cap,T1 cost) {
         edges.push_back((Edge){from, to, cap, 0, cost});
         edges.push_back((Edge){to, from, 0, 0, -cost});
         m = edges.size();
         G[from].push_back(m-2);
         G[to].push_back(m-1);
     }
-    bool SPFA(ll s, ll t, ll& tot_cost,ll& tot_flow) {
-        for(ll i = 0; i < n; i++) d[i] = INF;
-        memset(inq, 0, sizeof(inq));
-        d[s] = 0;
-        inq[s] = 1;
-        p[s] = 0;
-        a[s] = INF;
-        queue<ll> Q;
-        Q.push(s);
-        while(!Q.empty()) {
-            ll u = Q.front();
-            Q.pop();
-            inq[u] = 0;
-            for(ll i = 0; i < G[u].size(); i++) {
-                Edge& e = edges[G[u][i]];
-                if(e.cap > e.flow && d[e.to] > d[u] + e.cost) {
-                    d[e.to] = d[u] + e.cost;
-                    p[e.to] = G[u][i];
-                    a[e.to] = min(a[u], e.cap - e.flow);
-                    if(!inq[e.to]) {
-                        Q.push(e.to);
-                        inq[e.to] = 1;
+    pair<T1,T2> Mincost(int s,int t) {//bellmanFord
+        T1 tot_cost = 0;
+        T2 tot_flow = 0;
+        while(true) {
+            for(int i = 0; i < n; i++) d[i] = INF;
+            d[s] = 0;
+            p[s] = 0;
+            a[s] = inf;
+            bool up=true;
+            while(up) {
+                up=false;
+                for(int u = 0; u < n; u++) {
+                    if(d[u]-INF>=-eps)continue;
+                    for(int j:G[u]) {
+                        Edge &e=edges[j];
+                        if(e.cap > e.flow && d[e.to] > d[u] + e.cost+eps) {
+                            d[e.to] = d[u] + e.cost;
+                            p[e.to] = j;
+                            a[e.to] = min(a[u], e.cap - e.flow);
+                            up=true;
+                        }
                     }
                 }
             }
+            if(abs(d[t]-INF)<=eps)break;
+            tot_cost += (T1)d[t] * a[t];
+            tot_flow += (T2)a[t];
+            int u = t;
+            while(u != s) {
+                edges[p[u]].flow += a[t];
+                edges[p[u]^1].flow -= a[t];
+                u = edges[p[u]].from;
+            }
         }
-        if(d[t] == INF) return false;
-        tot_cost += (ll)d[t] * (ll)a[t];
-        tot_flow += (ll)a[t];
-        ll u = t;
-        while(u != s) {
-            edges[p[u]].flow += a[t];
-            edges[p[u]^1].flow -= a[t];
-            u = edges[p[u]].from;
-        }
-        return true;
+        return {tot_cost,tot_flow};
     }
-    pair<ll,ll> Mincost(ll s, ll t) {
-        ll cost = 0;
-        ll flow = 0;
-        while(SPFA(s, t, cost,flow));
-        return {cost,flow};
+    pair<T1,T2> Mincost2(int s,int t) {//SPFA
+        T1 tot_cost = 0;
+        T2 tot_flow = 0;
+        while(true) {
+            for(int i = 0; i < n; i++) d[i] = INF;
+            memset(inq, 0, sizeof(inq));
+            d[s] = 0;
+            inq[s] = 1;
+            p[s] = 0;
+            a[s] = inf;
+            queue<int> Q;
+            srand(time(NULL));
+            Q.push(s);
+            while(!Q.empty()) {
+                int u = Q.front();
+                Q.pop();
+                inq[u] = 0;
+                for(int i = 0; i < G[u].size(); i++) {
+                    Edge& e = edges[G[u][i]];
+                    if(e.cap > e.flow && d[e.to] > d[u] + e.cost+eps) {
+                        d[e.to] = d[u] + e.cost;
+                        p[e.to] = G[u][i];
+                        a[e.to] = min(a[u], e.cap - e.flow);
+                        if(!inq[e.to]) {
+                            Q.push(e.to);
+                            inq[e.to] = 1;
+                        }
+                    }
+                }
+            }
+            if(abs(d[t]-INF)<=eps)break;
+            tot_cost += (T1)d[t] * a[t];
+            tot_flow += a[t];
+            int u = t;
+            while(u != s) {
+                edges[p[u]].flow += a[t];
+                edges[p[u]^1].flow -= a[t];
+                u = edges[p[u]].from;
+            }
+        }
+
+        return {tot_cost,tot_flow};
     }
 } mcmf;
