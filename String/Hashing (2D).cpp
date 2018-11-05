0028877594e998b2a358af285d147e267a0ca66f
Anik Sarker <aniksarkerbuet1997@gmail.com>
2018-11-05 18:31:37 +0600

    Update Hashing (2D).cpp

--- String/Hashing (2D).cpp
+++ String/Hashing (2D).cpp
@@ -1,17 +1,22 @@
 #define MAX 2005
 #define ll long long int
+
 ll Base[2] = {773,709};
 ll Mod[2] = {281559881,693329647};
+
 ll InvBase[2][2];
 ll Pow[MAX][2][2],Inv[MAX][2][2];
 ll PowCell[MAX][MAX][2],InvCell[MAX][MAX][2];
+
 ll A[MAX][MAX],B[MAX][MAX];
 ll cA[MAX][MAX][2],cB[MAX][MAX][2];
+
 ll cMod(ll x,ll mod){
     while(x >= mod) x-=mod;
     while(x < 0)    x+=mod;
     return x;
 }
+
 ll bigMod(ll n,ll r,ll mod){
     if(r==0) return 1;
     ll ret = bigMod(n, r/2, mod);
@@ -19,18 +24,20 @@ ll bigMod(ll n,ll r,ll mod){
     if(r&1) ret = (ret*n)% mod;
     return ret;
 }
+
 ll invMod(ll n,ll mod) {return bigMod(n,mod-2,mod);}
+
 void Preprocess(){
     for(int b=0;b<2;b++) for(int m=0;m<2;m++)                         
         Pow[0][b][m]     = Inv[0][b][m] = 1;
-    for(int b=0;b<2;b++)for(int m=0;m<2;m++)for(int i=1;i<MAX;i++)
-        Pow[i][b][m]=(Pow[i-1][b][m]*Base[b]) % Mod[m];
+    for(int b=0;b<2;b++) for(int m=0;m<2;m++) for(int i=1;i<MAX;i++)
+        Pow[i][b][m]     = (Pow[i-1][b][m]*Base[b]) % Mod[m];
     for(int m=0;m<2;m++) for(int i=0;i<MAX;i++) for(int j=0;j<MAX;j++)
         PowCell[i][j][m] = (Pow[i][0][m]*Pow[j][1][m]) % Mod[m];
 
     for(int b=0;b<2;b++) for(int m=0;m<2;m++)                          
         InvBase[b][m]    = invMod(Base[b],Mod[m]);
-    for(int b=0;b<2;b++) for(int m=0;m<2;m++)   for(int i=1;i<MAX;i++) 
+    for(int b=0;b<2;b++) for(int m=0;m<2;m++) for(int i=1;i<MAX;i++) 
         Inv[i][b][m]     = (Inv[i-1][b][m]*InvBase[b][m]) % Mod[m];
     for(int m=0;m<2;m++) for(int i=0;i<MAX;i++) for(int j=0;j<MAX;j++) 
         InvCell[i][j][m] = (Inv[i][0][m]*Inv[j][1][m])%Mod[m];
