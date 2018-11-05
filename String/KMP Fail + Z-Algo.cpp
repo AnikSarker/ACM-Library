8da0193dff6deeca02a7ce419a379bed40ebc994
Anik Sarker <aniksarkerbuet1997@gmail.com>
2018-11-06 04:35:09 +0600

    Update KMP Fail + Z-Algo.cpp

--- String/KMP Fail + Z-Algo.cpp
+++ String/KMP Fail + Z-Algo.cpp
@@ -19,7 +19,7 @@ int KMPSearch(string txt, string pat){
         if(txt[i] == pat[j]) i++, j++;
 
         if(j == pat.size()) Count++, j = fail[j - 1];
-        else if(i < txt.size() && txt[i] != txt[j]){
+        else if(i < txt.size() && txt[i] != pat[j]){
             if(j != 0) j = fail[j - 1];
             else i++;
         }
