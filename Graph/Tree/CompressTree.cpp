e59ac9f8b8f2284e40e8d4b2e83b23612ecd20a4
Saad Muhammed Junayed <sm.junayed71@gmail.com>
2018-11-05 16:00:16 +0600

    Update CompressTree.cpp

--- Graph/Tree/CompressTree.cpp
+++ Graph/Tree/CompressTree.cpp
@@ -1,22 +1,24 @@
-//CompressTree , 0-rooted with specific nodes
-vpi compressTree(LCA& lca, const vector<int>& subset) {
-	vector<int> rev;
-  rev.resize(lca.dist.size());
-  vector<int> li = subset,&T = lca.time;
-	auto cmp = [&](int a, int b){return T[a] < T[b]; };
-  sort(li.begin(),li.end(), cmp);
-	int m = li.size()-1;
-	for(int i=0;i<m;i++) {
-		int a = li[i], b = li[i+1];
-    li.push_back(lca.query(a, b));
-	}
-	sort(li.begin(),li.end(), cmp);
-  li.erase(unique(li.begin(),li.end()), li.end());
-	for(int i=0;i<li.size();i++)rev[li[i]] = i;
-  vpi ret = {pair<int,int>(0, li[0])};
-	for(int i=0;i<li.size()-1;i++) {
-		int a = li[i], b = li[i+1];
-    ret.emplace_back(rev[lca.query(a, b)],b);
-	}
-	return ret;
+typedef vector<int> vi;
+typedef vector<pair<int,int> > vpi;
+vpi compressTree(const vector<int>& subset) {
+    vi rev(n+1);
+    vi li = subset;
+    auto cmp = [&](int a, int b) {
+        return st[a] < st[b];
+    };
+    sort(li.begin(),li.end(), cmp);
+    int m = li.size()-1;
+    for(int i=0; i<m; i++) {
+        int a = li[i], b = li[i+1];
+        li.push_back(lca(a, b));
+    }
+    sort(li.begin(),li.end(), cmp);
+    li.erase(unique(li.begin(),li.end()), li.end());
+    for(int i=0; i<li.size(); i++)rev[li[i]] = i;
+    vpi ret = {pair<int,int>(0, li[0])};
+    for(int i=0; i<li.size()-1; i++) {
+        int a = li[i], b = li[i+1];
+        ret.emplace_back(rev[lca(a, b)],b);
+    }
+    return ret;
 }
