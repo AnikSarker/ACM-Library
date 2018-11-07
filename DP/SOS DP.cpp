ec294d2dbe0f8f41938580dd58cdcf202e56a955
Anik Sarker <aniksarkerbuet1997@gmail.com>
2018-11-07 19:31:48 +0600

    Update SOS DP.cpp

--- DP/SOS DP.cpp
+++ DP/SOS DP.cpp
@@ -1,6 +1,6 @@
 ///SOS_DP
 
 if((mask & (1 << pos)) == true){
-    dp[pos][mask] = dp[pos - 1][mask] + dp[pos - 1][mask ^(1 << pos)]
+    dp[pos][mask] = dp[pos - 1][mask] + dp[pos - 1][mask ^ (1 << pos)]
 }
 else dp[pos][mask] = dp[pos - 1][mask];
