b5bfe835661fd64d649f1c32d4d3d215e90f69ef
Saad Muhammed Junayed <sm.junayed71@gmail.com>
2018-11-08 15:19:34 +0600

    Update FastIO.cpp

--- AdHoc/FastIO.cpp
+++ AdHoc/FastIO.cpp
@@ -1,10 +1,8 @@
 inline int getint(){
-    char ch=getchar();
-    int x=0;
+    char ch=getchar();int x=0;
     while(ch<'0' || ch>'9') ch=getchar();
     while(ch>='0' && ch<='9'){
-        x=x*10+ch-'0';
-        ch=getchar();
+        x=x*10+ch-'0';ch=getchar();
     }
     return x;
 }
