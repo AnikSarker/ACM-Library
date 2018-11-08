a03b637fe91a66a1eccfd006ec4e08c7c8296eb2
Anik Sarker <aniksarkerbuet1997@gmail.com>
2018-11-09 01:26:24 +0600

    Create Fast Fibonacci.cpp

---
+++ Math/Number Theory/Fast Fibonacci.cpp
@@ -0,0 +1,15 @@
+#define ll long long int
+
+pair<ll, ll> fib(ll n) {
+    if(n == 0) return {0, 1};
+    
+    ll x, y;
+    if(n & 1) {
+        tie(y, x) = fib(n - 1);
+        return {x, (y + x) % MOD};
+    }
+    else{
+        tie(x, y) = fib(n >> 1);
+        return {(x * y + x * (y - x + MOD)) % MOD, (x * x + y * y) % MOD};
+    }
+}
