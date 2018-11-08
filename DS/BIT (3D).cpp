47c8ed851486d695d8cf4619cf9de0c15fe87f8b
Anik Sarker <aniksarkerbuet1997@gmail.com>
2018-11-09 02:18:02 +0600

    Create BIT (3D).cpp

---
+++ DS/BIT (3D).cpp
@@ -0,0 +1,8 @@
+//Point Update and Point Range similar as 2D BIT
+
+void update(int x1, int y1, int z1, int x2, int y2, int z2){
+    update(x2, y2, z2, 1),          update(x1 - 1, y1 - 1, z2, 1);
+    update(x1 - 1, y2, z1 - 1, 1),  update(x2, y1 - 1, z1 - 1, 1);
+    update(x1 - 1, y2, z2, -1),     update(x2, y1 - 1, z2, -1);
+    update(x2, y2, z1 - 1, -1),     update(x1 - 1, y1 - 1, z1 - 1, -1);
+}
