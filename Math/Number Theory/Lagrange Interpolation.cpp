7847ee7a64b76c50c021b90dd14358b03731ed06
Anik Sarker <aniksarkerbuet1997@gmail.com>
2018-11-08 19:48:33 +0600

    Create Lagrange Interpolation.cpp

---
+++ Math/Number Theory/Lagrange Interpolation.cpp
@@ -0,0 +1,36 @@
+#define ll long long int
+#define MAXN 1000005
+#define MOD 1000000007
+
+ll bigMod(ll n, ll r) {
+    if (r==0)   return 1LL;
+    ll ret = bigMod (n, r/2);
+    ret = (ret * ret)% MOD;
+    if (r %2 ) ret = (ret * n) % MOD;
+    return ret;
+}
+
+ll Point[MAXN];
+ll Fact[MAXN];
+
+// Calculate first n points on the polynomial
+// n should be >= degree of the polynomial
+// Then find f(n) for any n using interpolation
+
+ll interpolate(int n,ll d) {
+    if(d<=n) return Point[d];
+
+    ll num = 1;
+    for(int i=0; i<=n; i++) num=(num * (d-i)) % MOD;
+
+    ll ret = 0;
+    for(int i=0; i<=n; i++) {
+        ll nn = (num * bigMod(d-i, MOD-2)) % MOD;
+        ll dd = (Fact[n-i] * Fact[i]) % MOD;
+
+        if((n-i) & 1) dd = MOD -dd;
+        nn = (Point[i] * nn) % MOD;
+        ret = (ret + nn * bigMod(dd, MOD-2))%MOD;
+    }
+    return ret;
+}
