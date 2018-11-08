74c435937b0f00fd5b1ab746540785b7ceb1dd1e
Saad Muhammed Junayed <sm.junayed71@gmail.com>
2018-11-08 15:20:50 +0600

    Update Knight Distance.cpp

--- AdHoc/Knight Distance.cpp
+++ AdHoc/Knight Distance.cpp
@@ -1,10 +1,6 @@
-#include<bits/stdc++.h>
-using namespace std;
 #define ll long long int
-
 // Minimum number of knight moves from (x,y) to
 // (0,0) in non-negative infinite chessboard
-
 ll knight_move(ll x, ll y){
     ll cnt = max({(x+1)/2, (y+1)/2, (x+y+2)/3});
     while((cnt%2) != (x+y)%2) cnt++;
