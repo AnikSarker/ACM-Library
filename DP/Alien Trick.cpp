bd386d95ff6cb1b4a31001c3d0daf3e16a4ff6d0
Anik Sarker <aniksarkerbuet1997@gmail.com>
2018-11-08 22:55:50 +0600

    Update Alien Trick.cpp

--- DP/Alien Trick.cpp
+++ DP/Alien Trick.cpp
@@ -1,5 +1,6 @@
-Lets say we need to divide n elements into k groups and minimize the sum of a specific function for each group.
-Normally this kind of problem can be solved by DP convex hull trick or Divide and Conquer trick.
+Lets say we need to divide n elements into k groups and
+minimize the sum of a specific function for each group.
+Normally this kind of problem can be solved by CHT or D&C.
 Alien trick comes into use when N, K <= 10^5.
 
 We can use alien trick if the following conditions holds:
@@ -16,9 +17,9 @@ we define G(n) = Fn(k) + kC
 That means we need to add an extra C for each group we use. 
 Now this G(n) function will follow a convex property.
 
-So now we can do binary search on C. Inside the binary search, for each mid,
-we find the optmial value of G(n) and the corresponding k for that value.
-This k decreases when C increases, and increases when C decreases.
+So now we can do binary search on C. Inside the binary search.
+For each mid, we find the optmial value of G(n) and the 
+corresponding k for that value. This k decreases when C increases.
 
-We will always find the lowest C for which the corresponding k is <= given K.
-This trick may apply for not only DP problems, but also other problems.
+We find the lowest C for which corresponding k is <= given K.
+This trick may apply for problems other than DP as well.
