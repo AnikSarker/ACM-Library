87158f92be7c559380c5aafa5e061ec3eea9132f
Anik Sarker <aniksarkerbuet1997@gmail.com>
2019-03-31 13:01:37 +0600

    Update 3D Geo.cpp

--- Geometry/3D Geo.cpp
+++ Geometry/3D Geo.cpp
@@ -7,95 +7,108 @@ inline int dcmp (double x) { if (fabs(x) < eps) return 0; else return x < 0 ? -1
 inline double torad(double deg) { return deg / 180 * pi; }
 
 struct Point{
+    double x, y;
+    Point (double x = 0, double y = 0): x(x), y(y) {}
+
+    Point operator + (const Point& u) { return Point(x + u.x, y + u.y); }
+    Point operator - (const Point& u) { return Point(x - u.x, y - u.y); }
+    Point operator * (const double u) { return Point(x * u, y * u); }
+    Point operator / (const double u) { return Point(x / u, y / u); }
+    double operator * (const Point& u) { return x*u.y - y*u.x; }
+};
+
+struct Point3D{
     double x, y, z;
-    Point() {}
+    Point3D() {}
     void read () {cin>>x>>y>>z;}
     void write () {cout<<x<<" --- "<<y<<" --- "<<z<<"\n";}
 
-    Point(double x, double y, double z) : x(x), y(y), z(z) {}
-    Point(const Point &p) : x(p.x), y(p.y), z(p.z) {}
-    Point operator +(Point b)  {return Point(x+b.x,y+b.y, z+b.z);}
-    Point operator -(Point b)  {return Point(x-b.x,y-b.y, z-b.z);}
-    Point operator *(double b) {return Point(x*b,y*b, z*b);}
-    Point operator /(double b) {return Point(x/b,y/b, z/b);}
-    bool operator  <(Point b)  {return make_pair(make_pair(x,y),z) < make_pair(make_pair(b.x,b.y),b.z);}
-    bool operator ==(Point b)  {return dcmp(x-b.x)==0 && dcmp(y-b.y) == 0 && dcmp(z-b.z) == 0;}
+    Point3D(double x, double y, double z) : x(x), y(y), z(z) {}
+    Point3D(const Point3D &p) : x(p.x), y(p.y), z(p.z) {}
+    Point3D operator +(Point3D b)  {return Point3D(x+b.x,y+b.y, z+b.z);}
+    Point3D operator -(Point3D b)  {return Point3D(x-b.x,y-b.y, z-b.z);}
+    Point3D operator *(double b) {return Point3D(x*b,y*b, z*b);}
+    Point3D operator /(double b) {return Point3D(x/b,y/b, z/b);}
+    bool operator  <(Point3D b)  {return make_pair(make_pair(x,y),z) < make_pair(make_pair(b.x,b.y),b.z);}
+    bool operator ==(Point3D b)  {return dcmp(x-b.x)==0 && dcmp(y-b.y) == 0 && dcmp(z-b.z) == 0;}
 };
-typedef Point Vector;
+typedef Point3D Vector3D;
+typedef vector<Point> Polygon;
+typedef vector<Point3D> Polyhedron;
 
 namespace Vectorial{
-    double getDot (Vector a, Vector b)  {return a.x*b.x+a.y*b.y+a.z*b.z;}
-    Vector getCross(Vector a, Vector b) {return Point(a.y*b.z-a.z*b.y, a.z*b.x-a.x*b.z, a.x*b.y-a.y*b.x);}
-    double getLength (Vector a)         {return sqrt(getDot(a, a)); }
-    double getPLength (Vector a)        {return getDot(a, a); }
-    Vector unitVector(Vector v)         {return v/getLength(v);}
+    double getDot (Vector3D a, Vector3D b)  {return a.x*b.x+a.y*b.y+a.z*b.z;}
+    Vector3D getCross(Vector3D a, Vector3D b) {return Point3D(a.y*b.z-a.z*b.y, a.z*b.x-a.x*b.z, a.x*b.y-a.y*b.x);}
+    double getLength (Vector3D a)         {return sqrt(getDot(a, a)); }
+    double getPLength (Vector3D a)        {return getDot(a, a); }
+    Vector3D unitVector(Vector3D v)         {return v/getLength(v);}
 
-    double getUnsignedAngle(Vector u,Vector v){
+    double getUnsignedAngle(Vector3D u,Vector3D v){
         double cosTheta = getDot(u,v)/getLength(u)/getLength(v);
         cosTheta = max(-1.0,min(1.0,cosTheta));
         return acos(cosTheta);
     }
 }
 
-struct Line{
-    Vector v; Point o;
-    Line(Vector v,Point o):v(v),o(o){}
-    Point getPoint(double t) {return o + v*t;}
+struct Line3D{
+    Vector3D v; Point3D o;
+    Line3D(Vector3D v,Point3D o):v(v),o(o){}
+    Point3D getPoint(double t) {return o + v*t;}
 };
 
 namespace Linear{
     using namespace Vectorial;
-    double getDistSq(Line l, Point p)          {return getPLength(getCross(l.v,p-l.o))/getPLength(l.v);}
-    double getDistLinePoint(Line l, Point p)   {return sqrt(getDistSq(l,p));}
-    bool cmp(Line l,Point p, Point q)          {return getDot(l.v,p) < getDot(l.v,q);}
-    Point projection(Line l,Point p)           {return l.o + l.v * getDot(l.v,p-l.o)/getPLength(l.v);}
-    Point reflection(Line l,Point p)           {return projection(l,p)+projection(l,p)-p;}
+    double getDistSq(Line3D l, Point3D p)          {return getPLength(getCross(l.v,p-l.o))/getPLength(l.v);}
+    double getDistLinePoint(Line3D l, Point3D p)   {return sqrt(getDistSq(l,p));}
+    bool cmp(Line3D l,Point3D p, Point3D q)          {return getDot(l.v,p) < getDot(l.v,q);}
+    Point3D projection(Line3D l,Point3D p)           {return l.o + l.v * getDot(l.v,p-l.o)/getPLength(l.v);}
+    Point3D reflection(Line3D l,Point3D p)           {return projection(l,p)+projection(l,p)-p;}
 
-    double getAngle(Line l,Line m)             {return getUnsignedAngle(l.v,m.v);}
-    bool isParallel(Line p,Line q)             {return dcmp(getPLength(getCross(p.v,q.v))) == 0;}
-    bool isPerpendicular(Line p,Line q)        {return dcmp(getDot(p.v,q.v)) == 0;}
+    double getAngle(Line3D l,Line3D m)             {return getUnsignedAngle(l.v,m.v);}
+    bool isParallel(Line3D p,Line3D q)             {return dcmp(getPLength(getCross(p.v,q.v))) == 0;}
+    bool isPerpendicular(Line3D p,Line3D q)        {return dcmp(getDot(p.v,q.v)) == 0;}
 
-    double getDist(Line l, Line m){
-        Vector n = getCross(l.v, m.v);
+    double getDist(Line3D l, Line3D m){
+        Vector3D n = getCross(l.v, m.v);
         if(getPLength(n) == 0) return getDistLinePoint(l,m.o);
         else return fabs(getDot(m.o-l.o , n)) / getLength(n);
     }
 
-    Point getClosestPointOnLine1(Line l,Line m){
-        Vector n = getCross(l.v, m.v);
-        Vector n2 = getCross(m.v, n);
+    Point3D getClosestPointOnLine1(Line3D l,Line3D m){
+        Vector3D n = getCross(l.v, m.v);
+        Vector3D n2 = getCross(m.v, n);
         return l.o + l.v * getDot(m.o-l.o, n2) / getDot(l.v, n2);
     }
 }
 
 struct Plane{
-    Vector n; //normal n
+    Vector3D n; //normal n
     double d; //getDot(n,p) = d for any point p on the plane
     Plane() {}
-    Plane(Vector n, double d) : n(n), d(d) {}
-    Plane(Vector n, Point p) : n(n), d(Vectorial :: getDot(n,p)) {}
+    Plane(Vector3D n, double d) : n(n), d(d) {}
+    Plane(Vector3D n, Point3D p) : n(n), d(Vectorial :: getDot(n,p)) {}
     Plane(const Plane &p) : n(p.n), d(p.d) {}
 };
 
 namespace Planar{
     using namespace Vectorial;
-    Plane getPlane(Point a,Point b,Point c) {return Plane(getCross(b-a,c-a),a);}
-    Plane translate(Plane p,Vector t)       {return Plane(p.n, p.d+getDot(p.n,t));}
+    Plane getPlane(Point3D a,Point3D b,Point3D c) {return Plane(getCross(b-a,c-a),a);}
+    Plane translate(Plane p,Vector3D t)       {return Plane(p.n, p.d+getDot(p.n,t));}
     Plane shiftUp(Plane p,double dist)      {return Plane(p.n, p.d+dist*getLength(p.n));}
     Plane shiftDown(Plane p,double dist)    {return Plane(p.n, p.d-dist*getLength(p.n));}
 
-    double getSide(Plane p,Point a)     {return getDot(p.n,a)-p.d;}
-    double getDistance(Plane p,Point a) {return fabs(getSide(p,a))/getLength(p.n);}
-    Point projection(Plane p,Point a)   {return a-p.n*getSide(p,a)/getPLength(p.n);}
-    Point reflection(Plane p,Point a)   {return a-p.n*getSide(p,a)/getPLength(p.n)*2;}
+    double getSide(Plane p,Point3D a)     {return getDot(p.n,a)-p.d;}
+    double getDistance(Plane p,Point3D a) {return fabs(getSide(p,a))/getLength(p.n);}
+    Point3D projection(Plane p,Point3D a)   {return a-p.n*getSide(p,a)/getPLength(p.n);}
+    Point3D reflection(Plane p,Point3D a)   {return a-p.n*getSide(p,a)/getPLength(p.n)*2;}
 
-    bool intersect(Plane p, Line l, Point& a){
+    bool intersect(Plane p, Line3D l, Point3D& a){
         if(dcmp(getDot(p.n,l.v)) == 0) return false;
         a = l.o - l.v * getSide(p,l.o) / getDot(p.n,l.v);
         return true;
     }
 
-    bool intersect(Plane p,Plane q,Line& l){
+    bool intersect(Plane p,Plane q,Line3D& l){
         l.v = getCross(p.n,q.n);
         if(dcmp(getPLength(l.v)) == 0) return false;
         l.o = getCross(q.n*p.d - p.n*q.d , l.v) / getPLength(l.v);
@@ -106,16 +119,16 @@ namespace Planar{
     bool isParallel(Plane p,Plane q)      {return dcmp(getPLength(getCross(p.n,q.n))) == 0;}
     bool isPerpendicular(Plane p,Plane q) {return dcmp(getDot(p.n,q.n)) == 0;}
 
-    bool getAngle(Plane p,Line l)         {return pi/2.0 - getUnsignedAngle(p.n,l.v);}
-    bool isParallel(Plane p,Line l)       {return dcmp(getDot(p.n,l.v)) == 0;}
-    bool isPerpendicular(Plane p,Line l)  {return dcmp(getPLength(getCross(p.n,l.v))) == 0;}
+    bool getAngle(Plane p,Line3D l)         {return pi/2.0 - getUnsignedAngle(p.n,l.v);}
+    bool isParallel(Plane p,Line3D l)       {return dcmp(getDot(p.n,l.v)) == 0;}
+    bool isPerpendicular(Plane p,Line3D l)  {return dcmp(getPLength(getCross(p.n,l.v))) == 0;}
 
-    Line perpThrough(Plane p,Point a)     {return Line(p.n,a);}
-    Plane perpThrough(Line l,Point a)     {return Plane(l.v,a);}
+    Line3D perpThrough(Plane p,Point3D a)     {return Line3D(p.n,a);}
+    Plane perpThrough(Line3D l,Point3D a)     {return Plane(l.v,a);}
 
     //Modify p.n if necessary with respect to the reference point
-    Vector rotateCCW90(Plane p,Vector d)  {return getCross(p.n,d);}
-    Vector rotateCW90(Plane p,Vector d)   {return getCross(d,p.n);}
+    Vector3D rotateCCW90(Plane p,Vector3D d)  {return getCross(p.n,d);}
+    Vector3D rotateCW90(Plane p,Vector3D d)   {return getCross(d,p.n);}
 
     pair<Point3D, Point3D> TwoPointsOnPlane(Plane p){
         Vector3D N = p.n; double D = p.d;
@@ -144,7 +157,7 @@ namespace Planar{
         return Point(getDot(v,X),getDot(v,Y));
     }
 
-    Point3D From2Dto3D(Plane p, Point a){
+    Point3D From2Dto3D(Plane p, Point3D a){
         auto Pair = TwoPointsOnPlane(p);
         Point3D A = Pair.first;
         Point3D B = Pair.second;
@@ -158,10 +171,10 @@ namespace Planar{
 }
 
 struct Sphere{
-    Point c;
+    Point3D c;
     double r;
     Sphere() {}
-    Sphere(Point c, double r) : c(c), r(r) {}
+    Sphere(Point3D c, double r) : c(c), r(r) {}
 
     //Spherical cap with polar angle theta
     double Height(double alpha)      {return r*(1-cos(alpha));}
@@ -190,25 +203,49 @@ namespace Spherical{
         return make_pair(SurfaceArea,Volume);
     }
 
-    Point getPointOnSurface(double r,double Lat,double Lon){
+    Point3D getPointOnSurface(double r,double Lat,double Lon){
         Lat = torad(Lat);  //North-South
         Lon = torad(Lon);  //East-West
-        return Point(r*cos(Lat)*cos(Lon), r*cos(Lat)*sin(Lon), r*sin(Lat));
+        return Point3D(r*cos(Lat)*cos(Lon), r*cos(Lat)*sin(Lon), r*sin(Lat));
     }
 
-    int intersect(Sphere s,Line l, vector<Point>& ret){
+    int intersect(Sphere s,Line3D l, vector<Point3D>& ret){
         double h2 = s.r*s.r - getDistSq(l,s.c);
         if(dcmp(h2)<0) return 0;
 
-        Point p = projection(l,s.c);
+        Point3D p = projection(l,s.c);
         if(dcmp(h2) == 0) {ret.push_back(p); return 1;}
 
-        Vector h = l.v * sqrt(h2) / getLength(l.v);
+        Vector3D h = l.v * sqrt(h2) / getLength(l.v);
         ret.push_back(p-h); ret.push_back(p+h); return 2;
     }
 
-    double GreatCircleDistance(Sphere s,Point a,Point b){
+    double GreatCircleDistance(Sphere s,Point3D a,Point3D b){
         return s.r * getUnsignedAngle(a-s.c, b-s.c);
+    }
+}
+
+namespace Poly{
+    using namespace Vectorial;
+    Sphere SmallestEnclosingSphere(Polyhedron p){
+        int n = p.size();
+        Point3D C(0,0,0);
+        for(int i=0; i<n; i++) C = C + p[i];
+        C = C / n;
+
+        double P = 0.1;
+        int pos = 0;
+        int Accuracy = 70000;
+        for (int i = 0; i < Accuracy; i++) {
+            pos = 0;
+            for (int j = 1; j < n; j++){
+                if(getPLength(C - p[j]) > getPLength(C - p[pos])) pos = j;
+            }
+
+            C = C + (p[pos] - C)*P;
+            P *= 0.998;
+        }
+        return Sphere(C, getPLength(C - p[pos]));
     }
 }
 
