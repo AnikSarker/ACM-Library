07dedae18f4bbc6882756bd7f6de7247a7591666
Anik Sarker <aniksarkerbuet1997@gmail.com>
2019-05-07 23:42:28 +0600

    Create __int128.cpp

---
+++ AdHoc/__int128.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n = 100;
+    __int128 Ans = n;
+
+	vector<char> vec;
+    vec.clear();
+    while(Ans) vec.push_back((int) Ans % 10), Ans /= 10;
+    reverse(vec.begin(),vec.end());
+    for(int x : vec) printf("%d",x);
+    printf("\n");
+}
