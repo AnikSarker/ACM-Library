1624fa82c485a2cd5b215d2b2d449a1e8a729c1b
Double-O <hrdd03@gmail.com>
2019-06-17 12:06:36 +0600

    Update Hashing.cpp

--- String/Hashing.cpp
+++ String/Hashing.cpp
@@ -1,31 +1,63 @@
-#define ll long long int
-#define MAX 100005
-
-string str;
+///Hashing
+/// Hashvalue(l...r) = hsh[l] - hsh[r + 1] * base ^ (r - l + 1);
+///Must call preprocess
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int MAX = 100009;
 ll mods[2] = {1000000007, 1000000009};
 ll bases[2] = {137, 281};
-ll Pow[2][MAX];
-ll Hash[2][MAX];
+ll pwbase[3][MAX];
+string str;
 
-// HashValue(l...r) = Hash[l] - Hash[r + 1] * base ^ (r - l + 1);
-pair < ll , ll > getHash(ll i, ll j){
-    ll tmp1 = (Hash[0][i] - (Hash[0][j + 1] * Pow[0][j - i + 1]) % mods[0]) % mods[0];
-    ll tmp2 = (Hash[1][i] - (Hash[1][j + 1] * Pow[1][j - i + 1]) % mods[1]) % mods[1];
-    if(tmp1 < 0) tmp1 += mods[0];
-    if(tmp2 < 0) tmp2 += mods[1];
-    return make_pair(tmp1, tmp2);
+
+void preprocess()
+{
+    pwbase[0][0] = pwbase[1][0] = 1;
+    for(ll i = 0; i < 2; i++){
+        for(ll j = 1; j < MAX; j++) pwbase[i][j] = (pwbase[i][j - 1] * bases[i]) % mods[i];
+    }
 }
 
-int main(){
-    Pow[0][0] = Pow[1][0] = 1;
-    for(int i=0;i<2;i++) for(int j=1; j < MAX; j++) Pow[i][j]= (Pow[i][j-1]*bases[i]) % mods[i];
+struct Hashing{
+    ll hsh[2][MAX];
+    string str;
 
-    str = "abcdabc";
-    int len = str.size();
-    for(int i = len - 1; i >= 0; i--){
-        for(int j = 0; j < 2; j++){
-            Hash[j][i] = (Hash[j][i + 1] * bases[j] + str[i]) % mods[j];
-            Hash[j][i] = (Hash[j][i] + mods[j]) % mods[j];
+    Hashing(){}
+    Hashing(string _str)
+    {
+        str = _str;
+        memset(hsh, 0, sizeof(hsh));
+        prep();
+    }
+
+    void prep()
+    {
+        for(ll i = str.size() - 1; i >= 0; i--){
+            for(int j = 0; j < 2; j++){
+                hsh[j][i] = (hsh[j][i + 1] * bases[j] + str[i]) % mods[j];
+                hsh[j][i] = (hsh[j][i] + mods[j]) % mods[j];
+            }
         }
     }
+
+    pair < ll , ll > getHash(ll i, ll j)
+    {
+        if(i > j) {
+            assert(false);
+//            return {0LL, 0LL};
+        }
+        ll tmp1 = (hsh[0][i] - (hsh[0][j + 1] * pwbase[0][j - i + 1]) % mods[0]) % mods[0];
+        ll tmp2 = (hsh[1][i] - (hsh[1][j + 1] * pwbase[1][j - i + 1]) % mods[1]) % mods[1];
+        if(tmp1 < 0) tmp1 += mods[0];
+        if(tmp2 < 0) tmp2 += mods[1];
+        return make_pair(tmp1, tmp2);
+    }
+
+};
+
+int main()
+{
+    preprocess();
+    return 0;
 }
