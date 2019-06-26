6b7f064593794925b870180b509700b4cb643490
Anik Sarker <aniksarkerbuet1997@gmail.com>
2019-06-27 03:33:45 +0600

    Update LCA (using SparseTable).cpp

--- Graph/Tree/LCA (using SparseTable).cpp
+++ Graph/Tree/LCA (using SparseTable).cpp
@@ -1,56 +1,34 @@
-//LCA using SparseTable
-struct Info{
-    int P;
-    ll Dist;
-    Info(){}
-    Info(int a,ll d){P=a;Dist=d;}
-}M[MAX][MaxLog];
-
-Info Merge(Info D,Info U) {Info now; now.P=U.P; now.Dist=D.Dist+U.Dist; return now;}
-
-void BuildSparse(){
+int Par[MAX], D[MAX];
+void BuildSparse(int n){
     //Using 0 for uninitialized parent (Remember and use carefully)
-    for(int i=1;i<=n;i++) for(int j=0;(1<<j)<n;j++) M[i][j]=Info(0,-1);
-	
-    for(int i=1;i<=n;i++) M[i][0]=Info(Par[i],Cost[i]);
+    for(int i=1;i<=n;i++) for(int j=0;(1<<j)<n;j++) P[i][j]=0;
+
+    for(int i=1;i<=n;i++) P[i][0]=Par[i];
     for(int j=1;(1<<j)<n;j++){
         for(int i=1;i<=n;i++){
-             if(M[i][j-1].P!=0){
-                int x=M[i][j-1].P;
-                M[i][j]=Merge(M[i][j-1],M[x][j-1]);
-            }
+             if(P[i][j-1]) {int x=P[i][j-1]; P[i][j]=P[x][j-1];}
         }
     }
 }
 
-Info LCA(int p,int q){
+int LCA(int p,int q){
     if (D[p]<D[q]) swap(p,q);
 
-    Info u=Info(p,0);
-    Info v=Info(q,0);
-
     int Log=log2(D[p])+1;
-    for(int i=Log;i>=0;i--) if(D[p]-D[q]>=(1<<i)) {u=Merge(u,M[p][i]); p=M[p][i].P;}
-    if(p==q) return u;
+    for(int i=Log;i>=0;i--) if(D[p]-D[q] >= (1<<i)) p=P[p][i];
+    if(p==q) return p;
 
-    for(int i=Log;i>=0;i--){
-        if(M[p][i].P!=0 && M[p][i].P!=M[q][i].P){
-            u=Merge(u,M[p][i]); p=M[p][i].P;
-            v=Merge(v,M[q][i]); q=M[q][i].P;
-        }
-    }
-    u=Merge(u,M[p][0]);
-    v=Merge(v,M[q][0]);
-	
+    for(int i=Log;i>=0;i--) if(P[p][i]!=0 && P[p][i] != P[q][i]) {p=P[p][i]; q=P[q][i];}
     int LCA=Par[p];
-    return Info(LCA,u.Dist+v.Dist);
-    //Note 1 : Reversing v while merging (u,v) might be necessary in some case.
-    //Note 2 : We  assign cost of an edge as the cost of the child node.
-    //Note 3 : In case values/costs for each node given (instead of each edge), merge u,M[LCA][0] and v together. 
-}
+    return LCA;
+    }
+
+//Note 1 : Reversing v while merging (u,v) might be necessary in some case.
+//Note 2 : We  assign cost of an edge as the cost of the child node.
+//Note 3 : In case values/costs for each node given (instead of each edge), merge u,M[LCA][0] and v together.
 
 int kth_par(int u,int d){
-    int Log=log2(d)+1;
-    for(int i=Log;i>=0;i--) if(d>=(1<<i)) {u=M[u][i].P; d-=(1<<i);}
+    int Log = log2(d)+1;
+    for(int i=Log;i>=0;i--) if(d >= (1<<i)) {u = M[u][i]; d -= (1<<i);}
     return u;
 }
