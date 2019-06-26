76b43b83ab1849b3ca42070433db4f6a91af2764
Anik Sarker <aniksarkerbuet1997@gmail.com>
2019-06-27 04:03:57 +0600

    Update MO & variations.cpp

--- DS/MO & variations.cpp
+++ DS/MO & variations.cpp
@@ -1,82 +1,57 @@
-//MO
-//Given an array of N integers indexed from 1 to N, and q queries, each in the form i j,
-//Find the number of distinct integers from index i to j (inclusive).
+//MO on Tree
+int Time = 0;
+int Euler[2*MAXN];
+vector<int> adj[MAXN];
+int Par[MAXN], D[MAXN], st[MAXN], en[MAXN];
 
-int S;
-int A[MAX];
-int Ans[MAX];
-bool vis[MAX];
-int Count[MAX];
-
-struct Q{
-    int L,R,id;
-    Q (){}
-    Q(int x,int y,int i){L=x;R=y;id=i;}
-    bool operator<(const Q other)const {
-      if (L/S!=other.L/S) return L/S<other.L/S;
-      return R>other.R;
+void dfs(int u,int p){
+    st[u] = ++Time; Euler[Time] = u;
+    Par[u] = p;
+    D[u] = D[p] + 1;
+    
+    for(int v : adj[u]){
+        if(v == p) continue;
+        dfs(v,u);
     }
-}my[MAX];
-
-int Unique;
-bool Check(int x){
-    if(!vis[x]) {vis[x]=1;  if(Count[A[x]]==0) Unique++; Count[A[x]]++;}
-    else        {vis[x]=0;  Count[A[x]]--; if(Count[A[x]]==0) Unique--;}
+    en[u] = ++Time; Euler[Time] = u;
 }
+using namespace MO;
+using namespace LCA;
 
 int main(){
-    int n,q,a,b;
-    int t;
-    scanf("%d",&t);
+    int N,Q;
+    scanf("%d %d",&N,&Q);
 
-    for(int cs=1;cs<=t;cs++){
-        scanf("%d %d",&n,&q);
-        Unique=0;
-        S=sqrt(n);
-        memset(vis,0,sizeof(vis));
-        memset(Count,0,sizeof(Count));
-        for(int i=1;i<=n;i++) scanf("%d",&A[i]);
-        for(int i=1;i<=q;i++) {scanf("%d %d",&a,&b); my[i]=Q(a,b,i);}
-        sort(my+1,my+q+1);
+    //Input
+    Time = 0;
+    dfs(1,0);
+    BuildSparse(N);
 
-        int left=my[1].L;
-        int right=my[1].L-1;
-        for(int i=1;i<=q;i++){
-            Q now=my[i];
-            while(left<now.L)  Check(left++);
-            while(left>now.L)  Check(--left);
-            while(right<now.R) Check(++right);
-            while(right>now.R) Check(right--);
-            Ans[my[i].id]=Unique;
-        }
-    }
-}
-
-//MO on Tree Path
-BuildSparse(){...}
-LCA(){...}
-
-int main(){
-    ......
-    for(int i=1;i<=m;i++){
+	for(int i=1;i<=Q;i++){
         int u,v;
         scanf("%d %d",&u,&v);
+
         if(st[u]>st[v]) swap(u,v);
         int lca=LCA(u,v);
-        if(lca==u) my[i]=Q(st[u],st[v],i);    //Case 1 : range(st[u],st[v])
-        else my[i]=Q(en[u],st[v],i);          //Case 2 : range(en[u],st[v]) and LCA(u,v)
+        if(lca == u) qry[i] = Query(st[u],st[v],i);   //Case 1 : range(st[u],st[v])
+        else qry[i] = Query(en[u],st[v],i);           //Case 2 : range(en[u],st[v]) and LCA(u,v)
     }
-    ......
-    for(int i=1;i<=m;i++){
-        Q now=my[i];
-        while(left<now.L)  Check(A[left++]);
-        while(left>now.L)  Check(A[--left]);
-        while(right<now.R) Check(A[++right]);
-        while(right>now.R) Check(A[right--]);
 
-        int lca=LCA(A[left],A[right]);
-        if(A[left]!=lca && A[right]!=lca) Check(lca); //Adding LCA(u,v) for Case 2
-        Ans1[my[i].id]=MaxFreq;
-        if(A[left]!=lca && A[right]!=lca) Check(lca); //Discarding the Addition
+    Sz = sqrt(N);
+    sort(qry+1,qry+Q+1);
+
+    int left = qry[1].L;
+    int right = left-1;
+    for(int i=1;i<=Q;i++){
+        Query now = qry[i];
+        while(left<now.L)  Check(Euler[left++]);
+        while(left>now.L)  Check(Euler[--left]);
+        while(right<now.R) Check(Euler[++right]);
+        while(right>now.R) Check(Euler[right--]);
+
+        int lca = LCA(Euler[left],Euler[right]);
+        if(Euler[left]!=lca && Euler[right]!=lca) Check(lca); //Adding LCA(u,v) for Case 2
+        Ans[now.id]=MaxFreq;
+        if(Euler[left]!=lca && Euler[right]!=lca) Check(lca); //Discarding the Addition
     }
 }
