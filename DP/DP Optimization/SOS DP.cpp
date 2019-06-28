69c04b8b2ee978cf3ca399ecfa96102b2506eb0c
Anik Sarker <aniksarkerbuet1997@gmail.com>
2019-06-29 01:53:19 +0600

    Update SOS DP.cpp

--- DP/DP Optimization/SOS DP.cpp
+++ DP/DP Optimization/SOS DP.cpp
@@ -3,12 +3,9 @@
 for(int mask = 0; mask < (1<<N); mask++){
     //handle base case separately (leaf states)
     dp[mask][-1] = A[mask];
-
     for(int b = 0; b < N; b++){
-        if(mask & (1<<b))
-            dp[mask][b] = dp[mask][b-1] + dp[mask^(1<<b)][b-1];
-        else
-            dp[mask][b] = dp[mask][b-1];
+        if(mask & (1<<b)) dp[mask][b] = dp[mask][b-1] + dp[mask^(1<<b)][b-1];
+        else              dp[mask][b] = dp[mask][b-1];
     }
     F[mask] = dp[mask][N-1];
 }
