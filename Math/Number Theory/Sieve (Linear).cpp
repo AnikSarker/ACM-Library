1de2714fd85bc4ec1f1075ed27a630cec56f09eb
Anik Sarker <aniksarkerbuet1997@gmail.com>
2019-07-16 09:08:18 +0600

    Update Sieve (Linear).cpp

--- Math/Number Theory/Sieve (Linear).cpp
+++ Math/Number Theory/Sieve (Linear).cpp
@@ -1,23 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
 const int MAX = 1000005;
 
 int least[MAX];
 vector<int> primes;
 
-void LinearSieve(int n) {
-  n = max(n, 1);
-  for(int i=0;i<=n;i++) least[i] = 0;
+void RunLinearSieve(int n) {
+    n = max(n, 1);
+    for(int i=0;i<=n;i++) least[i] = 0;
 
-  primes.clear();
-  for (int i = 2; i <= n; i++) {
-    if (least[i] == 0) {
-      least[i] = i;
-      primes.push_back(i);
+    primes.clear();
+    for (int i = 2; i <= n; i++) {
+        if (least[i] == 0) {
+            least[i] = i;
+            primes.push_back(i);
+        }
+        for (int x : primes) {
+            if (x > least[i] || i * x > n) break;
+            least[i * x] = x;
+        }
     }
-    for (int x : primes) {
-      if (x > least[i] || i * x > n) {
-        break;
-      }
-      least[i * x] = x;
-    }
-  }
 }
