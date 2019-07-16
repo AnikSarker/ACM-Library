587a60cedcc7f0964ef2885a8663c7bdda34387b
Anik Sarker <aniksarkerbuet1997@gmail.com>
2019-07-16 09:06:38 +0600

    Update Sieve (Linear).cpp

--- Math/Number Theory/Sieve (Linear).cpp
+++ Math/Number Theory/Sieve (Linear).cpp
@@ -1,12 +1,23 @@
-int Lp[MAX];
-vector<int>Primes;
+const int MAX = 1000005;
 
-void Sieve(){
-    for (int i=2; i < MAX; i++){
-        if(!Lp[i]) Lp[i] = i, Primes.push_back (i);
+int least[MAX];
+vector<int> primes;
 
-        for(int j=0; j< Primes.size() && Primes[j] <= Lp[i] && i * Primes[j] < MAX; j++){
-            Lp[ i * Primes[j] ] = Primes[j];
-        }
+void LinearSieve(int n) {
+  n = max(n, 1);
+  for(int i=0;i<=n;i++) least[i] = 0;
+
+  primes.clear();
+  for (int i = 2; i <= n; i++) {
+    if (least[i] == 0) {
+      least[i] = i;
+      primes.push_back(i);
     }
+    for (int x : primes) {
+      if (x > least[i] || i * x > n) {
+        break;
+      }
+      least[i * x] = x;
+    }
+  }
 }
