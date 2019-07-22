64b666c1787b7bec90c7773ef3f6e5a0d64a5b4d
Anik Sarker <aniksarkerbuet1997@gmail.com>
2019-07-22 16:40:03 +0600

    Update Sieve (Linear).cpp

--- Math/Number Theory/Sieve (Linear).cpp
+++ Math/Number Theory/Sieve (Linear).cpp
@@ -10,14 +10,25 @@ void RunLinearSieve(int n) {
     for(int i=0;i<=n;i++) least[i] = 0;
 
     primes.clear();
-    for (int i = 2; i <= n; i++) {
-        if (least[i] == 0) {
+    for(int i = 2; i <= n; i++){
+        if(least[i] == 0){
             least[i] = i;
             primes.push_back(i);
         }
-        for (int x : primes) {
-            if (x > least[i] || i * x > n) break;
+        for(int x : primes){
+            if(x > least[i] || i * x > n) break;
             least[i * x] = x;
         }
+    }
+}
+
+int mob[MAX];
+void Mobius(int n){
+    RunLinearSieve(n);
+    mob[1] = 1;
+    for(int i = 2; i <= n; i++) {
+        int j = i/least[i];
+        if(least[j] == least[i]) mob[i] = 0;
+        else mob[i] = -mob[j];
     }
 }
