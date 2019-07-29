d84a9a9bb07cdc3c9de9f5a4193d6426f7586b7b
Anik Sarker <aniksarkerbuet1997@gmail.com>
2019-07-30 01:30:59 +0600

    Update BIT (2D - Point).cpp

--- DS/BIT (2D - Point).cpp
+++ DS/BIT (2D - Point).cpp
@@ -1,27 +1,33 @@
-//2D BIT
-int BIT[MAX][MAX];
-void update(int x,int y,int val){  //A[x][y]+=val
-    int xx=x;
-    while(xx<MAX){
-        int yy=y;
-        while(yy<MAX){
-            BIT[xx][yy]+=val;
-            yy+=(yy&-yy);
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+const int MAXN = 1005;
+int BIT[MAXN][MAXN];
+
+//A[x][y] += val
+void update(int x, int y, int val){
+    int xx = x;
+    while(xx < MAXN){
+        int yy = y;
+        while(yy < MAXN){
+            BIT[xx][yy] += val;
+            yy += (yy & -yy);
         }
-        xx+=(xx&-xx);
+        xx += (xx & -xx);
     }
 }
 
-int sum(int x,int y){  //sum=SUM of all A[i][j], where 1<=i<=x and 1<=j<=y
-    int ans=0;
-    int xx=x;
-    while(xx!=0){
-        int yy=y;
-        while(yy!=0){
-            ans+=BIT[xx][yy];
-            yy-=(yy&-yy);
+//return sum over all A[i][j], where 1 <= i <= x and 1 <= j <= y
+int sum(int x, int y){
+    int ans = 0;
+    int xx = x;
+    while(xx != 0){
+        int yy = y;
+        while(yy != 0){
+            ans += BIT[xx][yy];
+            yy -= (yy & -yy);
         }
-        xx-=(xx&-xx);
+        xx -= (xx & -xx);
     }
     return ans;
 }
