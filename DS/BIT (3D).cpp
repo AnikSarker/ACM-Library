d00438e2cdf07af98e3ea5df9a560c474c2208ba
Anik Sarker <aniksarkerbuet1997@gmail.com>
2019-07-30 02:17:46 +0600

    Update BIT (3D).cpp

--- DS/BIT (3D).cpp
+++ DS/BIT (3D).cpp
@@ -1,4 +1,4 @@
-//Point Update and Point Range similar as 2D BIT
+//Point Update and Range Query similar as 2D BIT
 
 void update(int x1, int y1, int z1, int x2, int y2, int z2){
     update(x2, y2, z2, 1),          update(x1 - 1, y1 - 1, z2, 1);
