b132759543a6a5ac200191046f47e12371c31821
Anik Sarker <aniksarkerbuet1997@gmail.com>
2019-07-29 18:03:24 +0600

    Update and rename SparseTable (2D).cpp to SparseTable (2D - Square Query).cpp

---
+++ DS/SparseTable (2D - Square Query).cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//2D SparseTable
+const int MAXN = 1005;
+const int LOGN = 10;
+int A[MAXN][MAXN];
+int M[MAXN][MAXN][LOGN];
+
+//O(n^2 logn)
+//Supports Square Query
+void Build2DSparse(int n){
+    for(int k=0; (1<<k) <= n; k++){
+        for(int i=1; i+(1<<k)-1 <= n; i++){
+            for(int j=1; j+(1<<k)-1 <= n; j++){
+                if(k==0) M[i][j][k] = A[i][j];
+                else{
+                    int add = 1<<(k-1);
+                    int ret1 = max(M[i][j][k-1], M[i+add][j][k-1]);
+                    int ret2 = max(M[i][j+add][k-1], M[i+add][j+add][k-1]);
+                    M[i][j][k] = max(ret1, ret2);
+                }
+            }
+        }
+    }
+}
+
+//returns max of all A[i][j], where p<=i<=p+s-1 and q<=j<=q+s-1
+int Query(int p, int q, int s){
+    int k = log2(s);
+    int add = 1<<k;
+    int ret1 = max(M[p][q][k], M[p+s-add][q][k]);
+    int ret2 = max(M[p][q+s-add][k], M[p+s-add][q+s-add][k]);
+    return max(ret1, ret2);
+}
