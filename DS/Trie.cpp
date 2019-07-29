86b50244775b0e380bb1ae430b889c21c93875e8
Anik Sarker <aniksarkerbuet1997@gmail.com>
2019-07-29 17:09:00 +0600

    Update Trie.cpp

--- DS/Trie.cpp
+++ DS/Trie.cpp
@@ -1,28 +1,32 @@
 //Space Complexity : O(Sum of length of all strings)
 
-struct NODE{
-    NODE* Child[2];
-    int SIZE;
-    NODE(){
-        Child[0]=Child[1]=NULL;
-        SIZE=0;
+struct Trie{
+    struct Node{
+        Node* Child[2];
+        Node() {Child[0]=Child[1]=NULL;}
+    };
+    Node* root;
+
+    void Init() {root = new Node();}
+    bool Check(int x,int b) {return (x>>b)&1;}
+
+    void Insert(int n){
+        Node *Cur = root;
+        for(int i = LOG-1; i>=0; i--){
+            bool ID = Check(n,i);
+            if(!Cur->Child[ID]) Cur->Child[ID] = new Node();
+            Cur = Cur->Child[ID];
+        }
+    }
+
+    int Query(int XOR){
+        int Ans = 0;
+        Node *Cur = root;
+        for(int i = LOG-1; i>=0; i--){
+            bool ID = Check(XOR,i);
+            if(Cur->Child[ID]) Cur = Cur->Child[ID];
+            else Cur = Cur->Child[!ID], Ans += (1<<i);
+        }
+        return Ans;
     }
 };
-
-NODE* root;
-void INSERT(int n){
-    NODE *Cur=root;
-    for(int i=MAX-1;i>=0;i--)    {
-        int ID=check(n,i);
-        if(Cur->Child[ID]==NULL) Cur->Child[ID]=new NODE();
-        Cur=Cur->Child[ID];
-        Cur->SIZE++;
-    }
-}
-
-int QUERY(int L){
-    NODE *Cur=root;
-    int Ans=0;
-    for(int i=MAX-1; i>=0; i--){......}
-    return Ans;
-}
