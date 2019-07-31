12a2f4060edf39953bf17cd10da2b1d9a6bdb68b
Saad Muhammed Junayed <sm.junayed71@gmail.com>
2019-07-31 13:38:19 +0600

    Update and rename LinkCutTree3.cpp to LinkCutTree(rooted).cpp

--- Graph/Connectivity/LinkCutTree(rooted).cpp
+++ Graph/Connectivity/LinkCutTree(rooted).cpp
@@ -1,8 +1,5 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 struct Node{
-    int sz, label, w ;
+    ll sz, label, w ;
     Node *p, *pp, *l, *r;
     Node() { p = pp = l = r = 0; }
 };
@@ -117,7 +114,7 @@ Node *lca(Node *x, Node *y){
     return access(y);
 }
 
-int depth(Node *x){
+ll depth(Node *x){
     access(x);
     return x->sz;
 }
@@ -126,118 +123,23 @@ class LinkCut{
     Node *x;
 
     public:
-    void initLinkCut(int n,int *vat){
+    void initLinkCut(ll n,ll *vat){
         x = new Node[n+1];
-        for(int i = 1; i <= n; i++){
+        for(ll i = 1; i <= n; i++){
             x[i].label = i;
             x[i].w = vat[i];
             update(&x[i]);
         }
     }
-    virtual ~LinkCut(){   delete[] x;}
+    virtual ~LinkCut(){ delete[] x;}
+    //parent[u]=v.
+    void link(ll u, ll v){ ::link(&x[u], &x[v]);}
+    
+    void cut(ll u){ ::cut(&x[u]);}
+    
+    ll root(ll u){ return ::root(&x[u])->label;}
 
-    void link(int u, int v){   ::link(&x[u], &x[v]);}
+    ll depth(ll u){ return ::depth(&x[u]);}
 
-    void cut(int u){   ::cut(&x[u]);}
-
-    int root(int u){   return ::root(&x[u])->label;}
-
-    int depth(int u){   return ::depth(&x[u]);}
-
-    int lca(int u, int v){   return ::lca(&x[u], &x[v])->label;}
+    ll lca(ll u, ll v){ return ::lca(&x[u], &x[v])->label;}
 };
-LinkCut lct;
-int vat[100005];
-int cap;
-void solve(int cs){
-    int n , q ;
-    cin>>n>>q;
-    printf("Case %d:\n",cs);
-    cap=1;
-    for(int i=1;i<=n;i++)scanf("%d",&vat[i]);
-    lct.initLinkCut(n,vat);
-    for(int i=1;i<n;i++){
-        int u,v;
-        scanf("%d %d",&u,&v);
-        lct.link(u,v);
-    }
-    for(int i=1;i<=q;i++){
-        int type;
-        scanf("%d",&type);
-        int u , v ;
-        if(type==1){
-            scanf("%d",&u);
-            lct.cut(u);
-            lct.link(cap,u);
-            cap=u;
-            continue;
-        }
-        scanf("%d %d",&u,&v);
-        int lca = lct.lca(u,v);
-        int ans = lct.depth(u)+lct.depth(v)-2*lct.depth(lca)+vat[lca];
-        printf("%d %d\n",lca,ans);
-    }
-}
-int main()
-{
-    int t,cs=0;
-    cin>>t;
-    while(cs<t){
-        cs++;
-        solve(cs);
-    }
-
-    return 0;
-}
-
-/*
-
-
-/*
-4
-5 5
-1 2 3 4 5
-2 1
-3 1
-4 2
-5 2
-2 4 5
-1 2
-2 1 5
-1 5
-2 1 5
-5 5
-1 2 3 4 5
-2 1
-3 1
-4 2
-5 2
-2 4 5
-1 2
-2 1 5
-1 5
-2 1 5
-5 5
-1 2 3 4 5
-2 1
-3 1
-4 2
-5 2
-2 4 5
-1 2
-2 1 5
-1 5
-2 1 5
-5 5
-1 2 3 4 5
-2 1
-3 1
-4 2
-5 2
-2 4 5
-1 2
-2 1 5
-1 5
-2 1 5
-
-*/
