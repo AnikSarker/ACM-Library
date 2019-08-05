9ef1a759b4e069e7e852bc9405126f5379d60cb1
Double-O <hrdd03@gmail.com>
2019-08-05 19:32:10 +0600

    Update Red-Blue_Hackenbush.cpp

--- Game Theory/Red-Blue_Hackenbush.cpp
+++ Game Theory/Red-Blue_Hackenbush.cpp
@@ -1,12 +1,13 @@
-Lets say you are given a rooted tree and edges are coloured red and blue. Red player will cut red edges and blue player will cut blue edges.
-When a edge is cut, then the subtree under it is cut. 
+Lets say you are given a rooted tree and edges are coloured red and blue. Red player will cut red edges and blue player will cut blue
+edges.When a edge is cut, then the subtree under it is cut. 
 Who will win?
 
 It can be solved by red-blue hacken bush.
 
 If the tree is a chain, then it's simple. 
 Let cur = 1, and from root there are consecutive x blue/red edges. Then we add cur x times to our grundy val if the first edges are blue
-or minus cur x times if the first edges are red. And for every next edges we make cur = cur / 2 and add it to our grundy value depending on the edge colour.
+or minus cur x times if the first edges are red. And for every next edges we make cur = cur / 2 and add it to our grundy value depending
+on the edge colour.
 
 For a tree follow this pseudo-code:
 dfs(u):
