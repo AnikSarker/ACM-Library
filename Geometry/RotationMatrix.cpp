459c26fa9357163c8035789ab051b2a97ad47dec
Anik Sarker <aniksarkerbuet1997@gmail.com>
2019-08-07 01:52:23 +0600

    Update RotationMatrix.cpp

--- Geometry/RotationMatrix.cpp
+++ Geometry/RotationMatrix.cpp
@@ -9,7 +9,6 @@ Rotation Matrix :
 |  0      0     1 |
 
 Rotate a Point Q by  θ degrees counter-clockwise around the point P(x,y).  
-
 | cosθ   -sinθ   x(1-cosθ) + ysinθ | 
 | sinθ    cosθ   y(1-cosθ) - xsinθ |
 |  0        0             1        |
