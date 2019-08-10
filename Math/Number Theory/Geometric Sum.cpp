6e1081bf45b2f4153f19f0d4c0ab68ca3f3a2679
Anik Sarker <aniksarkerbuet1997@gmail.com>
2019-08-10 21:16:25 +0600

    Update Geometric Sum.cpp

--- Math/Number Theory/Geometric Sum.cpp
+++ Math/Number Theory/Geometric Sum.cpp
@@ -1,14 +1,14 @@
 //Computes the sum of a^1 + a^2 .... + a^n
 
-ll GeoSum(ll a,int n){
+ll GeoSum(ll a,ll n){
     ll sz = 0;
     ll ret = 0;
     ll mul = 1;
-    int MSB = 31 - __builtin_clz(n);
+    int MSB = 63 - __builtin_clzll(n);
  
     while(MSB >= 0){
-        ret = ret * (1 + mul); sz <<= 1; mul = (mul * mul) % MOD;
-        if((n>>MSB)&1) mul = (mul * a) % MOD, ret += mul;
+        ret = ret * (1 + mul); mul = (mul * mul) % MOD; sz <<= 1; 
+        if( (n >> MSB) & 1) {mul = (mul * a) % MOD; ret += mul; sz++;}
         ret %= MOD; MSB--;
     }
     return ret;
