39e0c9cc19f99d58df3108cede5ea040c7f6bd83
Anik Sarker <aniksarkerbuet1997@gmail.com>
2019-08-13 14:51:49 +0600

    Update Closest Pair of Points.cpp

--- DS/Closest Pair of Points.cpp
+++ DS/Closest Pair of Points.cpp
@@ -1,40 +1,45 @@
-// O(n (log n)^2)
-//Courtesy : Rezwan Arefin
-
+#include <bits/stdc++.h>
+using namespace std;
 #define ll long long int
-#define sq(a) ((a)*(a))
 
-struct point { 
-    ll x, y; 
-    bool operator < (const point &p) const {
-        return x == p.x ? y < p.y : x < p.x;
-    }
+struct Point{
+    ll x, y;
+    Point() {}
+    Point(ll _x, ll _y) {x = _x; y = _y;}
+    bool operator < (const Point &p) const {return x == p.x ? y < p.y : x < p.x;}
+    Point operator - (Point p) {return Point(x - p.x, y - p.y);}
 };
-ll dist(point &p, point &q) { return sq(p.x - q.x) + sq(p.y - q.y); }
-vector<point> p;
+ll getDot(Point a,Point b)  {return a.x * b.x + a.y * b.y;}
+ll dist(Point p, Point q)   {return getDot(p-q, p-q);}
+
+vector<Point> p;
 ll solve(int l, int r) {
     if(r - l <= 3) {
-        ll ret = 1e18;
-        for(int i = l; i <= r; i++) 
-            for(int j = i + 1; j <= r; j++) 
-                ret = min(ret, dist(p[i], p[j])); 
-        return ret; 
-    } 
-    int mid = l + r >> 1;
-    ll d = min(solve(l, mid), solve(mid+1, r)); 
+        ll ret = LLONG_MAX;
+        for(int i = l; i <= r; i++)
+            for(int j = i + 1; j <= r; j++)
+                ret = min(ret, dist(p[i], p[j]));
+        return ret;
+    }
 
-    vector<point> t;
-    for(int i = l; i <= r; i++) 
-        if(sq(p[mid].x - p[i].x) <= d) 
-            t.push_back({p[i].y, p[i].x}); 
-        
-    sort(t.begin(), t.end()); 
+    int mid = (l + r) / 2;
+    ll d = min(solve(l, mid), solve(mid+1, r));
+
+    vector<Point> t;
+    for(int i = l; i <= r; i++){
+        ll dx = p[mid].x - p[i].x;
+        if(dx * dx <= d) t.push_back({p[i].y, p[i].x});
+    }
+
+    sort(t.begin(), t.end());
     for(int i = 0; i < t.size(); i++) {
-        for(int j = i+1; j < t.size() && j <= i + 15; j++) 
-            d = min(d, dist(t[i], t[j])); 
-    } return d;
+        for(int j = i+1; j < t.size() && j <= i + 15; j++)
+            d = min(d, dist(t[i], t[j]));
+    }
+    return d;
 }
+
 ll closestPair() {
-    sort(p.begin(), p.end()); 
-    return solve(0, p.size()-1); 
+    sort(p.begin(), p.end());
+    return solve(0, p.size()-1);
 }
