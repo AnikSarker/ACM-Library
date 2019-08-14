3a27134bd90d9c2066cb0147c8bd770ccb2a791d
Anik Sarker <aniksarkerbuet1997@gmail.com>
2019-08-14 20:15:29 +0600

    Update NTT.cpp

--- Math/Polynomial/NTT.cpp
+++ Math/Polynomial/NTT.cpp
@@ -1,50 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
 const ll mod = 786433;
-// FOR 10^9 + 7 : {7340033, 415236097, 463470593} 
+//For 10^9 + 7 : {7340033, 415236097, 463470593}
+
+inline ll add(ll x, ll y) {x += y; return x >= mod ? x - mod : x;}
+inline ll sub(ll x, ll y) {x -= y; return x < 0    ? x + mod : x;}
+inline ll mul(ll x, ll y) {return (x * y) % mod;}
+inline ll Pow(ll x, ll y){
+    ll res = 1;
+    while(y){
+        if(y & 1) res = (res * x) % mod;
+        y >>= 1, x = (x * x) % mod;
+    }
+    return res;
+}
 
 struct NTT{
-    vector<ll>A,B;
-    vector<ll> w[2],rev;
+    vector<ll> A,B;
+    vector<ll> root, rev;
     ll P,M,G;
 
     NTT(ll mod) {P = mod; G = 10;}
-    ll Pow(ll a, ll b){
-        ll res=1;
-        for(;b; b>>=1,a=(a*a)%P) if (b&1) res=(res*a)%P;
-        return res;
-    }
-
     void init(ll n){
-        for(M=2; M<n; M<<=1);
-        M<<=1;
+        M = 1; int z = 0;
+        while(M < n) M <<= 1, z++;
+        M <<= 1; z++;
+
         A.resize(M); B.resize(M);
-        w[0].resize(M); w[1].resize(M); rev.resize(M);
-        for (ll i=0; i<M; i++){
-            ll x=i, &y=rev[i];
-            y=0;
-            for(ll k=1; k<M; k<<=1,x>>=1) (y<<=1)|=x&1;
-        }
-        ll x=Pow(G,(P-1)/M),y=Pow(x,P-2);
-        w[0][0]=w[1][0]=1;
-        for (ll i=1; i<M; i++){
-            w[0][i]=(w[0][i-1]*x)%P;
-            w[1][i]=(w[1][i-1]*y)%P;
-        }
+        root.resize(M); rev.resize(M);
+        for(ll i = 1; i < M; i++) rev[i] = rev[i >> 1] >> 1 | (i & 1) << (z - 1);
+
+        ll x = Pow(G, (P-1) / M);
+        root[0] = 1;
+        for(ll i = 1; i < M; i++) root[i] = (root[i-1] * x) % P;
     }
 
     void ntransform(vector<ll> &a,ll f){
-        for (ll i=0; i<M; i++)
-            if (i<rev[i]) swap(a[i],a[rev[i]]);
-        for (ll i=1; i<M; i<<=1)
-            for (ll j=0,t=M/(i<<1); j<M; j+=i<<1)
-                for (ll k=0,l=0; k<i; k++,l+=t){
-                    ll x=a[j+k+i]*1ll*w[f][l]%P;
-                    ll y=a[j+k];
-                    a[j+k+i] = y-x<0 ? y-x+P : y-x;
-                    a[j+k] = y+x>=P ? y+x-P : y+x;
+        if(f) reverse(a.begin() + 1, a.end());
+        for (ll i = 0; i < M; i++) if(i < rev[i]) swap(a[i],a[rev[i]]);
+        for (ll i = 1; i < M; i <<= 1){
+            for (ll j = 0, t = M / (i << 1); j < M; j += i << 1){
+                for (ll k = 0, l = 0; k < i; k++, l += t){
+                    ll x = a[j + k];
+                    ll y = mul(a[j + k + i], root[l]);
+                    a[j + k] = add(x, y);
+                    a[j + k + i] = sub(x, y);
                 }
-        if (f){
-            ll x=Pow(M,P-2);
-            for (ll i=0; i<M; i++) a[i]=a[i]*1ll*x%P;
+            }
+        }
+
+        if(f){
+            ll inv = Pow(M, P-2);
+            for(ll i = 0; i < M; i++) a[i] = mul(a[i], inv);
         }
     }
 
@@ -53,9 +61,9 @@ struct NTT{
         for (ll i = 0; i < M; i++) A[i] = B[i] = 0;
         for (ll i = 0; i < X.size(); i++) A[i] = X[i];
         for (ll i = 0; i < Y.size(); i++) B[i] = Y[i];
-        ntransform(A,0); ntransform(B,0);
+        ntransform(A, 0); ntransform(B, 0);
         res.clear(); res.resize(M);
-        for (ll i=0; i<M; i++) res[i]=A[i]*1LL*B[i]%P;
-        ntransform(res,1);
+        for (ll i = 0; i < M; i++) res[i] = mul(A[i], B[i]);
+        ntransform(res, 1);
     }
-};
+} ntt(mod);
