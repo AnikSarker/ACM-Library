bb77780aba4899bb5718c64289d3c8bb0ce89810
Anik Sarker <aniksarkerbuet1997@gmail.com>
2019-08-19 04:32:14 +0600

    Update Integer Tricks.cpp

--- Geometry/Integer Tricks.cpp
+++ Geometry/Integer Tricks.cpp
@@ -1,5 +1,5 @@
-//1. Checks the relation between a/b and c/d 
-bool comp(ll a, ll b, ll c, ll d){
+// 1. Checks the relation between a/b and c/d 
+bool comp (ll a, ll b, ll c, ll d){
   if(a / b != c / d) return a / b < c / d;
   a %= b, c %= d;
   if(c == 0) return 0;
@@ -7,14 +7,17 @@ bool comp(ll a, ll b, ll c, ll d){
   return comp(d, c, b, a);
 }
 
-//2. Sorting points based on angle
-bool sgn(Point p){
-  if(p.y != 0) return p.y < 0;
-  return p.x < 0;
-}
- 
+// 2. Sorting points based on polar angle
+bool sgn (Point p) {return p.y < 0 || (p.y == 0 && p.x < 0);}
+
 bool operator < (Point p, Point q) const{
   if(sgn(p) != sgn(q)) return sgn(p) < sgn(q);
   return getCross(p, q) > 0;
 }
 
+// 3. Check whether Angle(a1, b1) < Angle(a2, b2)
+bool angleLess (Vector a1, Vector b1, Vector a2, Vector b2) {
+    pt p1 ( getDot(a1, b1), abs(getCross(a1, b1)) );
+    pt p2 ( getDot(a2, b2), abs(getCross(a2, b2)) );
+    return getCross(p1, p2) > 0;
+}
