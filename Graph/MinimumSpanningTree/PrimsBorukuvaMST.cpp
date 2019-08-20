69caff40b4358f382f294b43d7f819a6b21cafde
Saad Muhammed Junayed <sm.junayed71@gmail.com>
2019-08-20 23:39:02 +0600

    Update and rename Prims & Borukuva's Algo.txt to PrimsBorukuvaMST.cpp

--- Graph/MinimumSpanningTree/PrimsBorukuvaMST.cpp
+++ Graph/MinimumSpanningTree/PrimsBorukuvaMST.cpp
@@ -3,15 +3,15 @@ MST : Prim’s Algorithm
 initial distance of all nodes are INFINITE except the starting node , which distance is 0.
 and all nodes are marked as non-visited.
 Now until all nodes are visited, pick the non-visited node u 
-which distance is lowest than all other non-visited nodes.After
- picking any nodes, update its all adjacent node v’s distance 
- if dis[u]+edgeCost(u,v)<dis[v] .  
+which distance is lowest than all other non-visited nodes.
+After picking any nodes, update its all adjacent node v’s distance 
+	 if dis[u]+edgeCost(u,v)<dis[v] .  
 
 MST : Borukuva’s Algorithm
 *all edge cost must be distinct.
 initially all nodes are individual components.
 While there is more than one components,
-	for each components	
-		find the smallest edge which connects this component 
-		to any other component. And then add this edge.
+  for each components	
+    find the smallest edge which connects this component 
+		 	to any other component. And then add this edge.
 These edges are the edges of minimum spanning tree. */
