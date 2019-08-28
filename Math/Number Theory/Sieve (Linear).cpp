397bb7b921f48e166c94ee4aaebf951ed9d725f6
Anik Sarker <aniksarkerbuet1997@gmail.com>
2019-08-29 00:18:40 +0600

    Update Sieve (Linear).cpp

--- Math/Number Theory/Sieve (Linear).cpp
+++ Math/Number Theory/Sieve (Linear).cpp
@@ -2,7 +2,7 @@
 using namespace std;
 const int MAX = 10000005;
 
-int phi[MAX], dvc[MAX], sig[MAX];
+int phi[MAX], dvc[MAX], sig[MAX], mob[MAX];
 int least[MAX], lstCnt[MAX], lstSum[MAX];
 vector<int> primes;
 
@@ -11,12 +11,12 @@ void RunLinearSieve(int n) {
     for(int i=0;i<=n;i++) least[i] = lstCnt[i] = lstSum[i] = 0;
 
     primes.clear();
-    phi[1] = dvc[1] = sig[1] = 1;
+    phi[1] = dvc[1] = sig[1] = mob[1] = 1;
 
     for(int i = 2; i <= n; i++){
         if(least[i] == 0){
             least[i] = i; lstCnt[i] = 1; lstSum[i] = 1 + i;
-            phi[i] = i - 1; dvc[i] = 2; sig[i] = 1 + i;
+            phi[i] = i - 1; dvc[i] = 2; sig[i] = 1 + i; mob[i] = -1;
             primes.push_back(i);
         }
         for(int x : primes){
@@ -30,6 +30,7 @@ void RunLinearSieve(int n) {
                 phi[i * x] = phi[i] * x;
                 dvc[i * x] = dvc[i] / (lstCnt[i] + 1) * (lstCnt[i * x] + 1);
                 sig[i * x] = sig[i] / lstSum[i] * lstSum[i * x];
+                mob[i * x] = 0;
             }
             else{
                 lstCnt[i * x] = 1;
@@ -38,25 +39,14 @@ void RunLinearSieve(int n) {
                 phi[i * x] = phi[i] * (x - 1);
                 dvc[i * x] = dvc[i] * 2;
                 sig[i * x] = sig[i] * (1 + x);
+                mob[i * x] = -mob[i];
             }
         }
-    }
-}
-
-int mob[MAX];
-void Mobius(int n){
-    RunLinearSieve(n);
-
-    mob[1] = 1;
-    for(int i = 2; i <= n; i++) {
-        int j = i/least[i];
-        if(least[j] == least[i]) mob[i] = 0;
-        else mob[i] = -mob[j];
     }
 }
 
 int main(){
     int n = 100;
     RunLinearSieve(n);
-    for(int i=1;i<=n;i++) cout<<i<<" "<<phi[i]<<" "<<dvc[i]<<" "<<sig[i]<<endl;
+    for(int i=1;i<=n;i++) cout<<i<<" "<<phi[i]<<" "<<dvc[i]<<" "<<sig[i]<<" "<<mob[i]<<endl;
 }
