fbb07673922ed3b843e12401d34579b30931b6cc
Anik Sarker <aniksarkerbuet1997@gmail.com>
2019-08-29 02:34:35 +0600

    Update Suffix Automata.cpp

--- String/Suffix Automata.cpp
+++ String/Suffix Automata.cpp
@@ -1,29 +1,30 @@
-//Tested : HDU 4270 - Dynamic Lover
-///Tested : Spoj SUBLEX, Spoj LCS, CF 128B
+// Tested : HDU 4270 - Dynamic Lover
+// Tested : SPOJ SUBLEX, SPOJ LCS, CF 128B
 
 #include<bits/stdc++.h>
 using namespace std;
-const int alphabetSize = 28;
+#define ll long long int
+const int ALPHA = 28;
 const int MAXLEN = 300005;
+int TotalLen, Size;
+int Root, Last;
 
-int TotalLen,Size;
-int Root,Last;
-
-///Dstnct_substr can be calculated online St[pos].Dstnct_substr = St[pos].Len - St[St[pos].link].Len
-///Occurence can be calculated online,
-///First, Every node pos except a clone node, St[pos].Occurrence = 1
-///then, St[pos].Occurence = sum of St[i].Occurrence where St[i].Link = pos
+// Dstnct_substr can be calculated online as St[pos].Dstnct_substr = St[pos].Len - St[St[pos].link].Len
+// Occurrence can be calculated online,
+// First, Every node pos except a clone node, St[pos].Occurrence = 1
+// then, St[pos].Occurence = sum of St[i].Occurrence where St[i].Link = pos
 
 struct Node{
-    int Link,Len;
-    long long Occurrence; ///How many times each state(endpos) occurs
-    long long Word;     ///How many substrings can be reached from this node
-    long long Dstnct_substr; ///How many distinct substrings can be reached from this node
-    int FirstPos,version,baseID;
-    int Next[alphabetSize];
+    int Link, Len;
+    ll Occurrence;    // How many times each state(endpos) occurs
+    ll Word;          // How many substrings can be reached from this node
+    ll Dstnct_substr; // How many distinct substrings can be reached from this node
+    int FirstPos, version, baseID;
+    int Next[ALPHA];
     void Clear(){
-        Len = Occurrence = Word = Dstnct_substr = 0; Link = baseID = FirstPos = version = -1;
-        memset(Next,0,sizeof(Next));
+        Len = Occurrence = Word = Dstnct_substr = 0;
+        Link = baseID = FirstPos = version = -1;
+        memset(Next, 0, sizeof(Next));
     }
 };
 
@@ -31,7 +32,6 @@ Node St[MAXLEN*2];
 bool isValid[MAXLEN*2];
 vector<int>CurrList;
 vector<int>LastList;
-
 
 inline void CreateNode(int dep){
     St[Size].Clear();
@@ -87,7 +87,7 @@ inline void SAM(int ch){
     Last = Curr;
 }
 
-inline void del(int len){
+inline void Del(int len){
     if(!len) return;
 
     for(int i = 0; i < len; i++){
@@ -99,17 +99,19 @@ inline void del(int len){
     }
 }
 
-inline void MarkTerminal(int u,int v = 1){
+inline void MarkTerminal(int u, int v = 1){
     while(u != -1) St[u].version = v,  u = St[u].Link, St[u].Occurrence = 1;
 }
 
-int FindSmallest(int len,int idx){
+// Returns Smallest Substring with length len
+// If i + len > s.size(), consider substring(i, s.size())
+int LexSmallestSubStr(int len, int idx){
     MarkTerminal(Last,idx);
 
     int cur = Root;
     for(int i= 0; i< len; i++){
         if(cur > Root && St[cur].version == idx) return TotalLen - i + 1;
-        for(int ch = 0; ch < alphabetSize; ch++){
+        for(int ch = 0; ch < ALPHA; ch++){
             if(!has(cur, ch )) continue;
             cur = St[cur].Next[ch];
             break;
@@ -117,31 +119,31 @@ int FindSmallest(int len,int idx){
     }
     return St[cur].FirstPos - len + 1;
 }
-///Returns longest common substring of 2 strings
-int LCS(char * s1, char * s2)
-{
+
+// Returns longest common substring of 2 strings
+int LCS(char * s1, char * s2){
     int len1 = strlen(s1), len2 = strlen(s2);
     init();
     for(int i = 0; i < len1; i++) SAM(s1[i] - 'a');
+
     int curNode = 0, curLen = 0, ans = 0;
     for(int i = 0; i < len2; i++){
-        while(curNode > -1 && St[curNode].Next[s2[i] - 'a'] == 0) curNode = St[curNode].Link, curLen = St[curNode].Len;
+        int ch = s2[i] - 'a';
+        while(curNode > -1 && St[curNode].Next[ch] == 0)
+            curNode = St[curNode].Link, curLen = St[curNode].Len;
+
         if(curNode == -1) curNode = 0;
-        if(St[curNode].Next[s2[i] - 'a']) curNode = St[curNode].Next[s2[i] - 'a'], curLen++;
+        if(St[curNode].Next[ch]) curNode = St[curNode].Next[ch], curLen++;
         ans = max(ans, curLen);
     }
     return ans;
 }
 
-///finds number of distinct substring
-///finds number of occurrence of every state(endpos class)
-///finds number of substring which can be reached from this node
-///must call MarkTerminal Before
-void dfs_sam(int pos)
-{
+// Must call MarkTerminal Before
+void dfs_sam(int pos){
     if(St[pos].Dstnct_substr) return;
     int resDstnct = 1;
-    for(int i = 0; i < alphabetSize; i++) if(St[pos].Next[i]) {
+    for(int i = 0; i < ALPHA; i++) if(St[pos].Next[i]){
         int to = St[pos].Next[i];
         dfs_sam(to);
         resDstnct += St[to].Dstnct_substr;
@@ -152,11 +154,8 @@ void dfs_sam(int pos)
     St[pos].Word += St[pos].Occurrence;
 }
 
-
-
-void PrintKthLexSubstr(int k)
-{
-    ///must call dfs_sam before
+void PrintKthLexSubstr(int k){
+    // Must call dfs_sam before
     int cur = Root;
     while(k > 0){
         int tmp = 0;
@@ -173,9 +172,8 @@ void PrintKthLexSubstr(int k)
     }
 }
 
-void PrintKthLexDstSubstr(int k)
-{
-    ///must call dfs_sam before
+void PrintKthLexDstSubstr(int k){
+    // Must call dfs_sam before
     int cur = Root;
     while(k > 0){
         int tmp = 0;
