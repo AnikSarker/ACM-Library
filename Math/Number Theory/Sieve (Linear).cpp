9f23d04b1cf588db599b8278ae38a3d0b9370453
Anik Sarker <aniksarkerbuet1997@gmail.com>
2019-08-29 00:01:02 +0600

    Update Sieve (Linear).cpp

--- Math/Number Theory/Sieve (Linear).cpp
+++ Math/Number Theory/Sieve (Linear).cpp
@@ -2,28 +2,43 @@
 using namespace std;
 const int MAX = 10000005;
 
-int phi[MAX];
-int least[MAX];
+int phi[MAX], dvc[MAX], sig[MAX];
+int least[MAX], lstCnt[MAX], lstSum[MAX];
 vector<int> primes;
 
 void RunLinearSieve(int n) {
     n = max(n, 1);
-    for(int i=0;i<=n;i++) least[i] = 0;
+    for(int i=0;i<=n;i++) least[i] = lstCnt[i] = lstSum[i] = 0;
 
-    phi[1] = 1;
     primes.clear();
+    phi[1] = dvc[1] = sig[1] = 1;
+
     for(int i = 2; i <= n; i++){
         if(least[i] == 0){
-            least[i] = i; lstCnt[i] = 1;
-            phi[i] = i - 1; sigma[i] = 2;
+            least[i] = i; lstCnt[i] = 1; lstSum[i] = 1 + i;
+            phi[i] = i - 1; dvc[i] = 2; sig[i] = 1 + i;
             primes.push_back(i);
         }
         for(int x : primes){
             if(x > least[i] || i * x > n) break;
             least[i * x] = x;
 
-            if(least[i] == x) phi[i * x] = phi[i] * x;
-            else phi[i * x] = phi[i] * (x - 1);
+            if(least[i] == x){
+                lstCnt[i * x] = lstCnt[i] + 1;
+                lstSum[i * x] = 1 + x * lstSum[i];
+
+                phi[i * x] = phi[i] * x;
+                dvc[i * x] = dvc[i] / (lstCnt[i] + 1) * (lstCnt[i * x] + 1);
+                sig[i * x] = sig[i] / lstSum[i] * lstSum[i * x];
+            }
+            else{
+                lstCnt[i * x] = 1;
+                lstSum[i * x] = 1 + x;
+
+                phi[i * x] = phi[i] * (x - 1);
+                dvc[i * x] = dvc[i] * 2;
+                sig[i * x] = sig[i] * (1 + x);
+            }
         }
     }
 }
@@ -38,4 +53,10 @@ void Mobius(int n){
         if(least[j] == least[i]) mob[i] = 0;
         else mob[i] = -mob[j];
     }
+}
+
+int main(){
+    int n = 100;
+    RunLinearSieve(n);
+    for(int i=1;i<=n;i++) cout<<i<<" "<<phi[i]<<" "<<dvc[i]<<" "<<sig[i]<<endl;
 }
