203b4024621f8b067b2d35c51e0644f2fcf83489
Anik Sarker <aniksarkerbuet1997@gmail.com>
2019-08-28 23:44:40 +0600

    Update Sieve (Linear).cpp

--- Math/Number Theory/Sieve (Linear).cpp
+++ Math/Number Theory/Sieve (Linear).cpp
@@ -1,23 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int MAX = 1000005;
+const int MAX = 10000005;
 
+int phi[MAX];
+int sigma[MAX];
 int least[MAX];
+int lstCnt[MAX];
 vector<int> primes;
 
 void RunLinearSieve(int n) {
     n = max(n, 1);
     for(int i=0;i<=n;i++) least[i] = 0;
 
+    phi[1] = 1;
     primes.clear();
     for(int i = 2; i <= n; i++){
         if(least[i] == 0){
-            least[i] = i;
+            least[i] = i; lstCnt[i] = 1;
+            phi[i] = i - 1; sigma[i] = 2;
             primes.push_back(i);
         }
         for(int x : primes){
             if(x > least[i] || i * x > n) break;
             least[i * x] = x;
+
+            if(least[i] == x) phi[i * x] = phi[i] * x;
+            else phi[i * x] = phi[i] * (x - 1);
         }
     }
 }
@@ -25,7 +33,7 @@ void RunLinearSieve(int n) {
 int mob[MAX];
 void Mobius(int n){
     RunLinearSieve(n);
-    
+
     mob[1] = 1;
     for(int i = 2; i <= n; i++) {
         int j = i/least[i];
