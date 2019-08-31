83dab8e37550b92a5f85b266320ebf72b5dadd4a
Anik Sarker <aniksarkerbuet1997@gmail.com>
2019-08-31 23:29:47 +0600

    Update Lehmer Prime Counting.cpp

--- Math/Number Theory/Lehmer Prime Counting.cpp
+++ Math/Number Theory/Lehmer Prime Counting.cpp
@@ -1,78 +1,46 @@
-//Tested : CF 665F - Four Divisors
-
-/* Let p(j) = j-th prime number.
-dp(n,j) = Count of k (1 <= k <= n and all prime div of k >= p(j))
-Then we have : dp(n,1) = n and dp(n,j+1) = dp(n,j) - dp(n/p(j),j)
-Let p(k) be the smallest prime > sqrt(n), π(n) = dp(n,k) + k - 1 */
-
 #include <bits/stdc++.h>
 using namespace std;
-
-#define MAXN 100
-#define MAXM 100010
-#define MAXP 666666
-#define MAX 10000010
 #define ll long long int
-#define chkbit(ar, i) (((ar[(i) >> 6]) & (1 << (((i) >> 1) & 31))))
-#define setbit(ar, i) (((ar[(i) >> 6]) |= (1 << (((i) >> 1) & 31))))
-#define isprime(x) (( (x) && ((x)&1) && (!chkbit(ar, (x)))) || ((x) == 2))
 
-namespace pcf{
-    ll dp[MAXN][MAXM];
-    unsigned int ar[(MAX>>6)+5] = {0};
-    int len=0, primes[MAXP], counter[MAX];
+const int LIM = 250;
+const int MAXP = 10000005;
 
-    void Sieve(){
-        setbit(ar,0), setbit(ar,1);
-        for(int i=3;(i*i)<MAX;i++,i++){
-            if(!chkbit(ar,i)){
-                int k=i<<1;
-                for(int j=(i*i);j<MAX;j+=k) setbit(ar,j);
-            }
-        }
+vector<int> P;
+int prec[MAXP];
+bool prime[MAXP];
 
-        for(int i=1;i<MAX;i++){
-            counter[i]=counter[i - 1];
-            if(isprime(i)) primes[len++]=i, counter[i]++;
+void build(){
+    prime[2] = true;
+    for(int i = 3; i < MAXP; i += 2) prime[i] = true;
+
+    for(int i = 3; i*i < MAXP; i += 2){
+        if(prime[i]){
+            for(int j = i*i; j < MAXP; j += i+i)
+                prime[j] = false;
         }
     }
 
-    void init(){
-        Sieve();
-        for(int n=0;n<MAXN;n++){
-            for(int m=0;m<MAXM;m++){
-                if(!n) dp[n][m]=m;
-                else dp[n][m]=dp[n-1][m]-dp[n-1][m/primes[n-1]];
-            }
-        }
-    }
+    for(int i=2; i<MAXP; i++) if(prime[i]) P.push_back(i);
+    for(int i=1; i<MAXP; i++) prec[i] = prec[i-1] + prime[i];
+}
 
-    ll phi(ll m,int n){
-        if(n==0) return m;
-        if(primes[n-1] >= m) return 1;
-        if(primes[n-1] * 1LL * primes[n-1] >=m) return counter[m] - n + 1;
-        if(m<MAXM && n<MAXN) return dp[n][m];
-        return phi(m,n-1) - phi(m/primes[n-1],n-1);
-    }
+ll dp[LIM*LIM][LIM];
+ll rec(ll N, int K){
+    if(N <= 1 || K < 0) return 0;
+    if(N <= P[K]) return N-1;
+    if(N < MAXP && P[K] * 1LL * P[K] > N) return N-1 - prec[N] + prec[P[K]];
 
-    //returns number of primes in range [1,m]
-    //Complexity : O(m^(2/3))
-    ll Lehmer(ll m){
-        if(m < MAX) return counter[m];
+    bool ok = N < LIM * LIM && K < LIM;
+    if (ok && dp[N][K]) return dp[N][K];
 
-        ll w,res=0;
-        int i,a,s,c,x,y;
-        s=sqrt(0.9+m), y=c=cbrt(0.9+m);
-        a=counter[y], res=phi(m,a)+a-1;
-        for(i=a;primes[i]<=s;i++) res=res-Lehmer(m/primes[i])+Lehmer(primes[i])-1;
-        return res;
-    }
-    
-    ll Legendre(ll m){
-        if (m < MAX) return counter[m];
+    ll ret = N/P[K] - rec(N/P[K], K-1) + rec(N, K-1);
 
-        int lim = sqrt(m) + 1;
-        int n = upper_bound(primes, primes + len, lim) - primes;
-        return phi(m, n) + (n - 1);
-     }
+    if(ok) dp[N][K] = ret;
+    return ret;
+}
+
+ll count_primes(ll N) {
+    if(N < MAXP) return prec[N];
+    int K = prec[(int)sqrt(N) + 1];
+    return N-1 - rec(N, K) + prec[P[K]];
 }
