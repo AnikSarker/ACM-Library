3b35f6e2cbf009b0e948e35607123b76cb770c09
Anik Sarker <aniksarkerbuet1997@gmail.com>
2019-09-24 01:26:49 +0600

    Update 2D Geo.cpp

--- Geometry/2D Geo.cpp
+++ Geometry/2D Geo.cpp
@@ -172,16 +172,17 @@ namespace Polygonal {
 
     int getConvexHull (Point* p, int n, Point* ch) {
         sort(p, p + n);
-        int m = 0;
-        for (int i = 0; i < n; i++) {
 
-            //while (m > 1 && dcmp(getCross(ch[m-1]-ch[m-2], p[i]-ch[m-1])) < 0) m--;
+        // preparing lower hull
+        int m = 0;
+        for (int i = 0; i < n; i++){
             while (m > 1 && dcmp(getCross(ch[m-1]-ch[m-2], p[i]-ch[m-1])) <= 0) m--;
             ch[m++] = p[i];
         }
+
+        // preparing upper hull
         int k = m;
-        for (int i = n-2; i >= 0; i--) {
-            //while (m > k && dcmp(getCross(ch[m-1]-ch[m-2], p[i]-ch[m-2])) < 0) m--;
+        for (int i = n-2; i >= 0; i--){
             while (m > k && dcmp(getCross(ch[m-1]-ch[m-2], p[i]-ch[m-2])) <= 0) m--;
             ch[m++] = p[i];
         }
