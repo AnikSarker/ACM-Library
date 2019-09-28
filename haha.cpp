1acf9b7c4d6646379e7536b18ccd339d3628b37a
Double-O <hrdd03@gmail.com>
2019-09-28 20:00:43 +0600

    Update haha.cpp

--- haha.cpp
+++ haha.cpp
@@ -1,89 +1,206 @@
 #include <bits/stdc++.h>
-using namespace std ;
+using namespace std;
+const int mod = 1000000007;
+const int maxn = 105;
 typedef long long ll;
-#define maxn 100005
-ll t, cs = 1, n;
-ll h[maxn];
+#define ACCEPTED 0
+#define REJECTED 1
+#define PENDING 2
 
-pair < ll , ll > getPos(ll idx, ll ttm)
-{
-    ll re1 = ttm % (2 * h[idx]);
-    if(re1 >= h[idx]) return make_pair(2 * h[idx] - re1, -1);
-    else return make_pair(re1, 1);
+struct data{
+    int state;
+    int time;
+    int run;
+    data(int s, int t, int r) {state = s, time = t, run = r;}
+
+    void Print(){
+        cout<<state<<" "<<time<<" "<<run<<endl;
+    }
+};
+
+data parse(string info){
+    data ret(0,0,0);
+    if(info[0] == '0') {ret.state = REJECTED; return ret;}
+
+    int idx = 0;
+    if(info[idx] == '?') ret.state = PENDING, idx++;
+    else if(info[idx] == '-') ret.state = REJECTED, idx++;
+    else ret.state = ACCEPTED;
+
+    int val = 0;
+    while(info[idx] != '/') {val = val * 10 + info[idx] - '0'; idx++;}
+    ret.run = val;
+
+    idx++;
+
+    val = 0;
+    while(idx < info.size() && info[idx] != ' ') {val = val * 10 + info[idx] - '0'; idx++;}
+    ret.time = val;
+    return ret;
 }
 
+string Names[maxn];
+vector<data> vec[maxn];
+vector<string> RankList;
+
+map < string , int > id;
+
+ll dp1[105][22][9000];
+ll dp2[105][22][9000];
+ll csum[105][22][9000];
+
 int main(){
-    cin >> t;
+    int t;
+    scanf("%d",&t);
 
-    while(t--){
+    for(int cs=1; cs<=t; cs++){
+        int n, m;
+        scanf("%d %d",&n, &m);
 
-        cin >> n;
-        for(ll i = 1; i <= n; i++) scanf("%lld", &h[i]);
+        memset(dp1, 0, sizeof(dp1));
+        memset(dp2, 0, sizeof(dp2));
+        memset(csum, 0, sizeof(csum));
 
-        ll anss = 0;
+        cin.ignore();
+        string str;
+        getline(cin, str);
+        getline(cin, str);
+        getline(cin, str);
 
-        for(ll i = 2; i <= n; i++){
+        int Count = 0;
+        for(char ch : str) if(ch == '|') Count++;
+        Count = Count - 3;
 
+        for(int i=1; i<=n; i++){
+            getline(cin, str);
 
-            pair < ll , ll > cur1 = getPos(i - 1, anss);
-            pair < ll , ll > cur2 = getPos(i, anss);
+            string name = "";
+            for(int j=1;j<21;j++) name += str[j];
+            Names[i] = name;
+            id[name] = i;
 
-            if(cur1.first >= cur2.first){
-                anss++;
-                continue;
+            int beg = 22;
+            for(int j=0; j<=m; j++){
+                string ToParse = "";
+                for(int k=0; k<7;k++) ToParse += str[beg+k];
+                data gelum = parse(ToParse);
+
+                beg += 8;
+                if(j > 0) vec[i].push_back(gelum);
             }
+            getline(cin, str);
+        }
 
-            if(h[i - 1] < h[i]){
-                if(cur2.second == 1){
-                    anss += h[i] - cur2.first + h[i] - h[i - 1];
-                    cur1 = getPos(i - 1, anss);
-                    cur2 = getPos(i, anss);
+        getline(cin, str);
+        getline(cin, str);
+        RankList.resize(n);
+
+        for(int i=1; i<=n; i++){
+            getline(cin, str);
+            string name = "";
+            for(int j=1;j<21;j++) name += str[j];
+            RankList.push_back(name);
+//            cout<<name<< ' ' << id[name] << endl;
+        }
+        getline(cin, str);
+
+        for(int i = 1; i <= n; i++){
+
+            for(int j = 0; j < (1 << m); j++){
+
+                bool flg = true;
+                int pen = 0;
+                for(int k = 0; k < m; k++){
+
+                    if(vec[i][k].state == ACCEPTED && ((1 << k) & j) == 0) {
+                        flg = false;
+                        break;
+                    }
+
+                    if(vec[i][k].state == REJECTED && ((1 << k) & j) == 1) {
+                        flg = false;
+                        break;
+                    }
+                    if((1 << k) & j) pen += vec[i][k].time + (vec[i][k].run - 1) * 20, cout << k << ' ';
                 }
-                else if(cur2.second == -1 && cur1.first > h[i - 1]){
-                    anss += cur1.first - h[i - 1];
-                    cur1 = getPos(i - 1, anss);
-                    cur2 = getPos(i, anss);
-                }
+                if(!flg) continue;
+                int slvcnt = __builtin_popcount(j);
+
+                dp1[i][slvcnt][pen]++;
+                cout << endl << i << ' ' << j << ' ' << slvcnt << ' ' << pen << endl;
+
             }
-            if(cur1.first >= cur2.first){
-                anss++;
-                continue;
-            }
-
-
-            ll x = cur1.first;
-            ll y = h[i] - cur2.first;
-            ll z = cur2.first - x;
-            ll p;
-
-
-            if(cur1.second == -1 && cur2.second == 1){
-                p = abs(x - y) + (z + 1) / 2;
-            }
-            else if(cur1.second == 1 && cur2.second == 1){
-                p = y + (z + 1) / 2;
-            }
-            else if(cur1.second == 1 && cur2.second == -1){
-                p = (z + 1) / 2;
-            }
-            else if(cur1.second == -1 && cur2.second == -1){
-                p = x + (z + 1) / 2;
-            }
-            else assert(false);
-
-            anss += p + 1;
-
-//            cout << "yo : " << i << ' ' << x << ' ' << y << ' ' << z << ' ' << p << endl;
-//            cout << "yo : " << i << ' ' << cur1.first << ' ' << cur1.second << ' ' << cur2.first << ' ' << cur2.second << endl;
-
-//            cout << i << ' ' << anss << endl;
 
         }
 
-        printf("Case %lld: %lld\n", cs++, anss);
+        int idd = id[RankList[0]];
+
+        for(int j = 1; j <= m; j++) {
+            for(int k = 8600; k >= 1; k--){
+                dp2[1][j][k] = dp1[idd][j][k];
+                csum[1][j][k] = csum[1][j][k + 1] + dp2[1][j][k];
+                if(csum[1][j][k] >= mod) csum[1][j][k] -= mod;
+            }
+        }
+
+        for(int i = 2; i <= n; i++){
+
+            int curid = id[RankList[i - 1]];
+
+            for(int myslv = 1; myslv <= m; myslv++){
+
+                for(int mypen = 1; mypen <= 8600; mypen++){
+
+                    int curanss = csum[i - 1][myslv][mypen];
+
+                    for(int j = myslv + 1; j <= m; j++){
+                        curanss = (curanss + csum[i - 1][j][1]);
+                        if(curanss >= mod) curanss -= mod;
+                    }
+
+                    curanss = (curanss * 1LL * dp1[curid][myslv][mypen]) % mod;
+
+                    dp2[i][myslv][mypen] = curanss;
+
+                }
+
+                for(int j = 8600; j >= 1; j--) {
+                    csum[i][myslv][j] = csum[i][myslv][j + 1] + dp2[i][myslv][j];
+                    if(csum[i][myslv][j] >= mod) csum[i][myslv][j] -= mod;
+                }
+            }
+
+        }
+
+        int anss = 0;
+        for(int i = 1; i <= m; i++) {
+            anss = anss + csum[n][i][1];
+        }
+
+        anss = anss % mod;
+
+        printf("Case %d: %d", cs++, anss);
 
     }
 
-
-    return 0;
 }
+
+/*
+1
+3 4
+|--------------------|-------|-------|-------|-------|-------|
+|Team Name           |Total  |A      |B      |C      |D      |
+|--------------------|-------|-------|-------|-------|-------|
+|Team X              |2/330  |?10/255|0/--   |1/10   |5/240  |
+|--------------------|-------|-------|-------|-------|-------|
+|Team Y              |2/330  |5/230  |0/--   |1/20   |?5/270 |
+|--------------------|-------|-------|-------|-------|-------|
+|Team Z              |1/30   |-4/--  |1/30   |?1/241 |?1/245 |
+|--------------------|-------|-------|-------|-------|-------|
+
+|--------------------|
+|Team Z              |
+|Team X              |
+|Team Y              |
+|--------------------|
+*/
