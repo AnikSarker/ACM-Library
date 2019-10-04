fa71c4261a41c271f180bdd6a1ce9c7607916c19
Anik Sarker <aniksarkerbuet1997@gmail.com>
2019-10-05 05:16:26 +0600

    Update Divide & Conquer Optimization.cpp

--- DP/DP Optimization/Divide & Conquer Optimization.cpp
+++ DP/DP Optimization/Divide & Conquer Optimization.cpp
@@ -6,26 +6,28 @@
 
 #include<bits/stdc++.h>
 using namespace std;
-#define MAX 5005
 #define ll long long int
+const int MAX = 5005;
+
 int ara[MAX];
-ll C[MAX][MAX],dp[2][MAX];
+ll C[MAX][MAX];
+ll dp[2][MAX];
 
-void compute(int K,int L,int R,int OptL,int OptR){
+void compute(int K, int L, int R, int OptL, int OptR){
     if(L > R) return;
-    int mid=(L+R)/2;
-    int optNow=mid-1;
+    int mid = (L+R)/2;
 
-    dp[K&1][mid]=0;
-    for(int i=OptL;i<=min(OptR, mid);i++){
-        ll tmp=dp[(K&1)^1][i-1]+C[i][mid];
-        if(tmp >= dp[K&1][mid]){
-            dp[K&1][mid]=tmp;
-            optNow=i;
+    int optNow = -1;
+    dp[K & 1][mid] = 0;
+    for(int i=OptL; i<=min(OptR, mid); i++){
+        ll tmp = dp[(K & 1) ^ 1][i - 1] + C[i][mid];
+        if(tmp >= dp[K & 1][mid]){
+            dp[K & 1][mid] = tmp;
+            optNow = i;
         }
     }
-    compute(K,L,mid-1,OptL,optNow);
-    compute(K,mid+1,R,optNow,OptR);
+    compute(K, L, mid - 1, OptL, optNow);
+    compute(K, mid + 1, R, optNow, OptR);
 }
 
 int main(){
@@ -33,15 +35,19 @@ int main(){
     scanf("%d",&t);
 
     while(t--){
-        int n,K; scanf("%d %d", &n, &K);
-        for(int i=1;i<=n;i++) scanf("%d",&ara[i]);
+        int n, K;
+        scanf("%d %d", &n, &K);
+        for(int i=1; i<=n; i++) scanf("%d",&ara[i]);
 
-        for(int i=1;i<=n;i++) for(int j=i;j<=n;j++){
-            if(i==j) C[i][j]=ara[j];
-            else C[i][j] = C[i][j-1]|ara[j];
+        for(int i=1; i<=n; i++){
+            for(int j=i; j<=n; j++){
+                if(i==j) C[i][j] = ara[j];
+                else C[i][j] = C[i][j-1] | ara[j];
+            }
         }
-        for(int i=1;i<=n;i++) dp[1][i]=C[1][i];
-        for(int i=2;i<=K;i++) compute(i,1,n,1,n);
-        printf("%lld\n",dp[K&1][n]);
+
+        for(int i=1; i<=n; i++) dp[1][i] = C[1][i];
+        for(int i=2; i<=K; i++) compute(i, 1, n, 1, n);
+        printf("%lld\n",dp[K & 1][n]);
     }
 }
