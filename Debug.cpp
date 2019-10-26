5884e79ea45b2e8410c2135baaaf2a124cd68d62
Saad Muhammed Junayed <sm.junayed71@gmail.com>
2019-10-26 22:12:37 +0600

    Create Debug.cpp

---
+++ Debug.cpp
@@ -0,0 +1,7 @@
+void debug_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+  cerr << " " << to_string(H);
+  debug_out(T...);
+}
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
